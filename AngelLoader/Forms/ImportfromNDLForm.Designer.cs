6e93ff2af67439575170b5747cc8d652cede8dd7
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-03-20 22:23:36 -0700

    Add preliminary NDL import form

---
+++ AngelLoader/Forms/ImportfromNDLForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace AngelLoader.Forms
+{
+    partial class ImportfromNDLForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseNDLIniFilesLabel = new System.Windows.Forms.Label();
+            this.Thief1GroupBox = new System.Windows.Forms.GroupBox();
+            this.Thief2GroupBox = new System.Windows.Forms.GroupBox();
+            this.Thief3GroupBox = new System.Windows.Forms.GroupBox();
+            this.Thief1IniTextBox = new System.Windows.Forms.TextBox();
+            this.Thief1IniBrowseButton = new System.Windows.Forms.Button();
+            this.Thief2IniBrowseButton = new System.Windows.Forms.Button();
+            this.Thief2IniTextBox = new System.Windows.Forms.TextBox();
+            this.Thief3IniBrowseButton = new System.Windows.Forms.Button();
+            this.Thief3IniTextBox = new System.Windows.Forms.TextBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.Cancel_Button = new System.Windows.Forms.Button();
+            this.Thief1GroupBox.SuspendLayout();
+            this.Thief2GroupBox.SuspendLayout();
+            this.Thief3GroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChooseNDLIniFilesLabel
+            // 
+            this.ChooseNDLIniFilesLabel.AutoSize = true;
+            this.ChooseNDLIniFilesLabel.Location = new System.Drawing.Point(16, 8);
+            this.ChooseNDLIniFilesLabel.Name = "ChooseNDLIniFilesLabel";
+            this.ChooseNDLIniFilesLabel.Size = new System.Drawing.Size(167, 13);
+            this.ChooseNDLIniFilesLabel.TabIndex = 0;
+            this.ChooseNDLIniFilesLabel.Text = "Choose NewDarkLoader.ini file(s):";
+            // 
+            // Thief1GroupBox
+            // 
+            this.Thief1GroupBox.Controls.Add(this.Thief1IniBrowseButton);
+            this.Thief1GroupBox.Controls.Add(this.Thief1IniTextBox);
+            this.Thief1GroupBox.Location = new System.Drawing.Point(8, 32);
+            this.Thief1GroupBox.Name = "Thief1GroupBox";
+            this.Thief1GroupBox.Size = new System.Drawing.Size(536, 64);
+            this.Thief1GroupBox.TabIndex = 1;
+            this.Thief1GroupBox.TabStop = false;
+            this.Thief1GroupBox.Text = "Thief 1";
+            // 
+            // Thief2GroupBox
+            // 
+            this.Thief2GroupBox.Controls.Add(this.Thief2IniBrowseButton);
+            this.Thief2GroupBox.Controls.Add(this.Thief2IniTextBox);
+            this.Thief2GroupBox.Location = new System.Drawing.Point(8, 104);
+            this.Thief2GroupBox.Name = "Thief2GroupBox";
+            this.Thief2GroupBox.Size = new System.Drawing.Size(536, 72);
+            this.Thief2GroupBox.TabIndex = 1;
+            this.Thief2GroupBox.TabStop = false;
+            this.Thief2GroupBox.Text = "Thief 2";
+            // 
+            // Thief3GroupBox
+            // 
+            this.Thief3GroupBox.Controls.Add(this.Thief3IniBrowseButton);
+            this.Thief3GroupBox.Controls.Add(this.Thief3IniTextBox);
+            this.Thief3GroupBox.Location = new System.Drawing.Point(8, 184);
+            this.Thief3GroupBox.Name = "Thief3GroupBox";
+            this.Thief3GroupBox.Size = new System.Drawing.Size(536, 72);
+            this.Thief3GroupBox.TabIndex = 1;
+            this.Thief3GroupBox.TabStop = false;
+            this.Thief3GroupBox.Text = "Thief 3";
+            // 
+            // Thief1IniTextBox
+            // 
+            this.Thief1IniTextBox.Location = new System.Drawing.Point(16, 24);
+            this.Thief1IniTextBox.Name = "Thief1IniTextBox";
+            this.Thief1IniTextBox.Size = new System.Drawing.Size(432, 20);
+            this.Thief1IniTextBox.TabIndex = 0;
+            // 
+            // Thief1IniBrowseButton
+            // 
+            this.Thief1IniBrowseButton.Location = new System.Drawing.Point(448, 23);
+            this.Thief1IniBrowseButton.Name = "Thief1IniBrowseButton";
+            this.Thief1IniBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.Thief1IniBrowseButton.TabIndex = 1;
+            this.Thief1IniBrowseButton.Text = "Browse...";
+            this.Thief1IniBrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // Thief2IniBrowseButton
+            // 
+            this.Thief2IniBrowseButton.Location = new System.Drawing.Point(447, 33);
+            this.Thief2IniBrowseButton.Name = "Thief2IniBrowseButton";
+            this.Thief2IniBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.Thief2IniBrowseButton.TabIndex = 3;
+            this.Thief2IniBrowseButton.Text = "Browse...";
+            this.Thief2IniBrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // Thief2IniTextBox
+            // 
+            this.Thief2IniTextBox.Location = new System.Drawing.Point(15, 34);
+            this.Thief2IniTextBox.Name = "Thief2IniTextBox";
+            this.Thief2IniTextBox.Size = new System.Drawing.Size(432, 20);
+            this.Thief2IniTextBox.TabIndex = 2;
+            // 
+            // Thief3IniBrowseButton
+            // 
+            this.Thief3IniBrowseButton.Location = new System.Drawing.Point(447, 33);
+            this.Thief3IniBrowseButton.Name = "Thief3IniBrowseButton";
+            this.Thief3IniBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.Thief3IniBrowseButton.TabIndex = 3;
+            this.Thief3IniBrowseButton.Text = "Browse...";
+            this.Thief3IniBrowseButton.UseVisualStyleBackColor = true;
+            // 
+            // Thief3IniTextBox
+            // 
+            this.Thief3IniTextBox.Location = new System.Drawing.Point(15, 34);
+            this.Thief3IniTextBox.Name = "Thief3IniTextBox";
+            this.Thief3IniTextBox.Size = new System.Drawing.Size(432, 20);
+            this.Thief3IniTextBox.TabIndex = 2;
+            // 
+            // OKButton
+            // 
+            this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OKButton.Location = new System.Drawing.Point(384, 312);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 2;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_Button
+            // 
+            this.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel_Button.Location = new System.Drawing.Point(464, 312);
+            this.Cancel_Button.Name = "Cancel_Button";
+            this.Cancel_Button.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_Button.TabIndex = 2;
+            this.Cancel_Button.Text = "Cancel";
+            this.Cancel_Button.UseVisualStyleBackColor = true;
+            // 
+            // ImportfromNDLForm
+            // 
+            this.AcceptButton = this.OKButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Cancel_Button;
+            this.ClientSize = new System.Drawing.Size(550, 345);
+            this.Controls.Add(this.Cancel_Button);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.Thief3GroupBox);
+            this.Controls.Add(this.Thief2GroupBox);
+            this.Controls.Add(this.Thief1GroupBox);
+            this.Controls.Add(this.ChooseNDLIniFilesLabel);
+            this.Name = "ImportfromNDLForm";
+            this.Text = "Import from NewDarkLoader";
+            this.Thief1GroupBox.ResumeLayout(false);
+            this.Thief1GroupBox.PerformLayout();
+            this.Thief2GroupBox.ResumeLayout(false);
+            this.Thief2GroupBox.PerformLayout();
+            this.Thief3GroupBox.ResumeLayout(false);
+            this.Thief3GroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ChooseNDLIniFilesLabel;
+        private System.Windows.Forms.GroupBox Thief1GroupBox;
+        private System.Windows.Forms.GroupBox Thief2GroupBox;
+        private System.Windows.Forms.GroupBox Thief3GroupBox;
+        private System.Windows.Forms.Button Thief1IniBrowseButton;
+        private System.Windows.Forms.TextBox Thief1IniTextBox;
+        private System.Windows.Forms.Button Thief2IniBrowseButton;
+        private System.Windows.Forms.TextBox Thief2IniTextBox;
+        private System.Windows.Forms.Button Thief3IniBrowseButton;
+        private System.Windows.Forms.TextBox Thief3IniTextBox;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button Cancel_Button;
+    }
+}
