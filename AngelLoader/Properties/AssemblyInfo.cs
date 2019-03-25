cad3028371dfb301dc5f3690694bae3b8a799471
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-03-25 03:44:21 -0700

    Properties tweaking

--- AngelLoader/Properties/AssemblyInfo.cs
+++ AngelLoader/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AngelLoader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A fan mission loader and manager for Thief series games.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AngelLoader")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
