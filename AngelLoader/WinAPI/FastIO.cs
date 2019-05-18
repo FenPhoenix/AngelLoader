0323a46d49931d6fd071d0fdbf0dffd0027a92ac
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-05-18 15:03:12 -0700

    Cleanup

--- AngelLoader/WinAPI/FastIO.cs
+++ AngelLoader/WinAPI/FastIO.cs
@@ -5,8 +5,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AngelLoader.WinAPI
 {
