acd1d228386545bc0dfe4933e0746da4ae2ecb1e
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-05-24 17:02:20 -0700

    Add overflow check... meh

--- AngelLoader/CustomControls/RichTextBoxCustom.cs
+++ AngelLoader/CustomControls/RichTextBoxCustom.cs
@@ -466,7 +466,19 @@ namespace AngelLoader.CustomControls
                 // Exponential scroll like most apps do - somewhat arbitrary values but has a decent feel.
                 // Clamp to 1 (pixel) to correct for loss of precision in divide where it could end up as 0 for
                 // too long.
-                int increment = ((delta * delta).Clamp(0, int.MaxValue) / 640).Clamp(1, 1024);
+
+                // OCD
+                int deltaSquared;
+                try
+                {
+                    deltaSquared = checked(delta * delta);
+                }
+                catch (OverflowException)
+                {
+                    deltaSquared = int.MaxValue;
+                }
+
+                int increment = (deltaSquared / 640).Clamp(1, 1024);
 
                 scrollIncrementY = cursY < origY ? -increment : increment;
 
