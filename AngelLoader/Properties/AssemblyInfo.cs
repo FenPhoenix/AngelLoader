3811e1d2c8a95e9d587979763e00e5c3098d3e03
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-10-12 04:12:33 -0700

    v1.1.5

--- AngelLoader/Properties/AssemblyInfo.cs
+++ AngelLoader/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyInformationalVersion("1.1.5 beta1")]
+[assembly: AssemblyInformationalVersion("1.1.5")]
 [assembly: NeutralResourcesLanguage("en-US")]
