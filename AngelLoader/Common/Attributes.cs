a89e71eee9d0171b51e1ab45ade6f2263333f051
FenPhoenix <FenPhoenix@users.noreply.github.com>
2019-10-25 02:34:01 -0700

    Mark FenGen attributes as "don't compile"

--- AngelLoader/Common/Attributes.cs
+++ AngelLoader/Common/Attributes.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 using JetBrains.Annotations;
 
 namespace AngelLoader.Common
 {
     internal static class Attributes
     {
+        // All attributes marked with a conditional based on a define that doesn't exist, so they won't be compiled
+        // (we only need these for pre-build code generation)
+
+        [Conditional("compile_FenGen_attributes")]
         [AttributeUsage(AttributeTargets.Class)]
         internal class FenGenLocalizationClassAttribute : Attribute { }
 
+        [Conditional("compile_FenGen_attributes")]
         [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
         internal class FenGenCommentAttribute : Attribute
         {
@@ -15,6 +21,7 @@ namespace AngelLoader.Common
         }
 
         // Yes, Roslyn is so bonkers-idiotic that I have to make an entire attribute just for this. Amazing.
+        [Conditional("compile_FenGen_attributes")]
         [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
         internal class FenGenBlankLineAttribute : Attribute
         {
@@ -22,6 +29,7 @@ namespace AngelLoader.Common
             public FenGenBlankLineAttribute([UsedImplicitly] int numberOfBlankLines) { }
         }
 
+        [Conditional("compile_FenGen_attributes")]
         [AttributeUsage(AttributeTargets.Class)]
         internal class FenGenLocalizationReadWriteClass : Attribute { }
     }
