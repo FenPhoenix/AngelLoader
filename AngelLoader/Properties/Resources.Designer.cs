0e7745cc1a99d75772550b2bdda0d055c1b3e326
FenPhoenix <FenPhoenix@users.noreply.github.com>
2020-02-16 01:47:04 -0800

    Add preliminary recent icon

--- AngelLoader/Properties/Resources.Designer.cs
+++ AngelLoader/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace AngelLoader.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FilterByRecentlyAdded {
+            get {
+                object obj = ResourceManager.GetObject("FilterByRecentlyAdded", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap FilterByReleaseDate {
             get {
                 object obj = ResourceManager.GetObject("FilterByReleaseDate", resourceCulture);
