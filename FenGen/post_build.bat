52da6afe873dd9894f971349250fdaf0d56fb282
FenPhoenix <FenPhoenix@users.noreply.github.com>
2020-06-10 02:06:02 -0700

    Really really really fix resx exclude gen (I think?!)
    But now we have to rebuild solution, or else it doesn't run. So... there's that. Fortunately I always rebuild solution out of habit anyway, but...

--- FenGen/post_build.bat
+++ FenGen/post_build.bat
@@ -1,4 +1,4 @@
-echo ------------------------- START OF pre_build.bat
+echo ------------------------- START OF FenGen/post_build.bat
 
 rem ~ strips surrounded quotes if they exist
 rem batch file hell #9072: no spaces can exist around = sign for these lines
@@ -13,9 +13,9 @@ rem Autogenerate code
 rem ---
 set FenGen="%SolutionDir%FenGen\bin\Release\net472\FenGen.exe"
 
-set fenGenArgs=-fmdata -language_t
+rem Run the resx exclude BEFORE AngelLoader even gets to its build (and AFTER FenGen has already been built).
+rem That way we should end up with a proper project file and no "sometimes works, sometimes doesn't" mess.
+set fenGenArgs=-exclude_resx
 
 %FenGen% %fenGenArgs%
 rem ---
-
-"%system%xcopy" "%SolutionDir%temp_transfer_bin\*.*" "%TargetDir%" /y /i
