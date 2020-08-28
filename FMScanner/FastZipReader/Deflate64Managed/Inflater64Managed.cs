34c4d71e34997d3ad8bfa20a433846d6303de134
FenPhoenix <FenPhoenix@users.noreply.github.com>
2020-08-27 20:38:04 -0700

    Throw up our hands and suppress nullable warnings in Deflate64Managed
    It clearly works in its present state, so just leave it be

--- FMScanner/FastZipReader/Deflate64Managed/Inflater64Managed.cs
+++ FMScanner/FastZipReader/Deflate64Managed/Inflater64Managed.cs
@@ -1,3 +1,8 @@
+// Fen's note: This is straight out of .NET Core 3 with no functional changes. I'm trusting it to be correct and
+// working, and I'm not gonna touch it even for nullability.
+
+#nullable disable
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
