48431e137e002ffb2cda383d750e57d236c6e14d
FenPhoenix <FenPhoenix@users.noreply.github.com>
2020-10-05 23:04:07 -0700

    Add Export fm.ini form

---
+++ AngelLoader/Forms/ExportFMIniForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AngelLoader.Forms
+{
+    public partial class ExportFMIniForm : Form
+    {
+        public ExportFMIniForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
