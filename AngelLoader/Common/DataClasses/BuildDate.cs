339e71436ef21d45a571f0771698650bfdce4b23
FenPhoenix <FenPhoenix@users.noreply.github.com>
2020-11-14 20:44:43 -0800

    1.4.5 beta 1

--- AngelLoader/Common/DataClasses/BuildDate.cs
+++ AngelLoader/Common/DataClasses/BuildDate.cs
@@ -7,6 +7,6 @@ namespace AngelLoader.DataClasses
     [FenGenBuildDateDestClass]
     internal static class BuildDateSource
     {
-        internal const string BuildDate = "20201115042339";
+        internal const string BuildDate = "20201115044352";
     }
 }
