6d0c29a32237b561bd4b8ecdccde48c8ee4dc4ff
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-02-15 14:07:23 -0800

    Cleanup

--- AngelLoader/Forms/MainForm.cs
+++ AngelLoader/Forms/MainForm.cs
@@ -42,7 +42,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -50,7 +49,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
