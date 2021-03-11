be23b4a41f9672e0ba1b484660c380f8216cdb99
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-03-10 20:15:53 -0800

    Preliminary totally custom datetimepicker work

---
+++ AngelLoader/Forms/CustomControls/DarkDateTimePicker2.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace AngelLoader.Forms.CustomControls
+{
+    partial class DarkDateTimePicker2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5, 3, 0, 0);
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "4/21/2004";
+            // 
+            // DarkDateTimePicker2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(2147483647, 20);
+            this.MinimumSize = new System.Drawing.Size(0, 20);
+            this.Name = "DarkDateTimePicker2";
+            this.Size = new System.Drawing.Size(160, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
