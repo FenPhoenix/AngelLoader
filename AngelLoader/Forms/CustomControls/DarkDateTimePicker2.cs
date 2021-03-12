7bc0e5ba6f95574c08f343f138e39601d8c2f3b3
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-03-11 18:05:00 -0800

    Add preliminary MonthCalendar dropdown

--- AngelLoader/Forms/CustomControls/DarkDateTimePicker2.cs
+++ AngelLoader/Forms/CustomControls/DarkDateTimePicker2.cs
@@ -12,6 +12,37 @@ namespace AngelLoader.Forms.CustomControls
 {
     public partial class DarkDateTimePicker2 : UserControl
     {
+        private sealed class CalendarDropDown : ToolStripDropDown
+        {
+            internal readonly MonthCalendar Calendar;
+
+            internal CalendarDropDown()
+            {
+                // Must do this to enable the border-remove hack below
+                AutoSize = false;
+
+                Calendar = new MonthCalendar { MaxSelectionCount = 1 };
+
+                var host = new ToolStripControlHost(Calendar);
+
+                Items.Add(host);
+            }
+
+            protected override void OnOpened(EventArgs e)
+            {
+                // Hack to get rid of the border and edges (Padding, Margin, Dock, etc. for any and all subcontrols
+                // is futile and does nothing)
+                Location = new Point(Location.X + 1, Location.Y - 1);
+                Calendar.Location = new Point(-1, -1);
+                Size = new Size(Calendar.Width - 2, Calendar.Height - 2);
+
+                // Must do this explicitly otherwise we don't get focus
+                Calendar.Focus();
+
+                base.OnOpened(e);
+            }
+        }
+
         public DarkDateTimePicker2()
         {
             InitializeComponent();
