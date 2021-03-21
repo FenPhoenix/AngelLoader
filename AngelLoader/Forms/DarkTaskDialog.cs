0f39adcc2264c3798e261a11251acc0aa666e0bc
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-03-20 22:09:51 -0700

    Cleanup

--- AngelLoader/Forms/DarkTaskDialog.cs
+++ AngelLoader/Forms/DarkTaskDialog.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using AngelLoader.DataClasses;
 using AngelLoader.Forms.CustomControls;
@@ -15,19 +9,15 @@ using static AngelLoader.Misc;
 
 namespace AngelLoader.Forms
 {
-    /*
-    Notes:
-    -Icon is at x:10, y:10, and size is 32x32
-    -Min width is 350, and max width is the greater of 350 and the bottom panel width (which is all the bottom
-     panel controls and their paddings combined)
-    -Text is x: Icon.Right + 10, or in other words 52 (Icon.Left (10) + Icon.Width (32) + 10 == 52),
-             y: Icon.Top + 8 (or 18) (at least it looks like that visually, we can check later)
-    -Allow up to 3 buttons (yes, no, cancel)
-    -Allow an optional checkbox
-    */
     public sealed partial class DarkTaskDialog : DarkForm
     {
+        #region Private fields
+
         private readonly Dictionary<Control, (Color ForeColor, Color BackColor)> _controlColors = new();
+
+        #endregion
+
+        #region Public fields
 
         [PublicAPI]
         public bool IsVerificationChecked => VerificationCheckBox.Checked;
@@ -39,6 +29,8 @@ namespace AngelLoader.Forms
             No,
             Cancel
         }
+
+        #endregion
 
         [PublicAPI]
         public DarkTaskDialog(
@@ -52,6 +44,9 @@ namespace AngelLoader.Forms
             bool? checkBoxChecked = null,
             Button defaultButton = Button.Cancel)
         {
+            // All numbers are just matching the original Win32 task dialog as closely as possible. Don't worry
+            // about them.
+
 #if DEBUG
             InitializeComponent();
 #else
@@ -69,21 +64,6 @@ namespace AngelLoader.Forms
 
             #endregion
 
-            // All numbers are just matching the original Win32 task dialog as closely as possible. Don't worry
-            // about them.
-
-            int imageMarginX = icon != MessageBoxIcon.None ? 49 : 7;
-
-            MessageLabel.Location = new Point(imageMarginX, MessageLabel.Location.Y);
-
-            Text = title;
-            MessageLabel.Text = message;
-
-            if (yesText != null) YesButton.Text = yesText;
-            if (noText != null) NoButton.Text = noText;
-            if (cancelText != null) Cancel_Button.Text = cancelText;
-            if (checkBoxText != null) VerificationCheckBox.Text = checkBoxText;
-
             // Have to use these bools, because if we check Visible it will always be false even though we might
             // just have set it to true, because we haven't shown ourselves yet so everything counts as not visible
             bool yesButtonVisible = yesText != null;
@@ -96,11 +76,29 @@ namespace AngelLoader.Forms
                 throw new ArgumentException("At least one button must have text specified!");
             }
 
+            int imageMarginX = icon != MessageBoxIcon.None ? 49 : 7;
+
+            #region Set control state
+
+            Text = title;
+            MessageLabel.Text = message;
+
+            if (yesText != null) YesButton.Text = yesText;
+            if (noText != null) NoButton.Text = noText;
+            if (cancelText != null) Cancel_Button.Text = cancelText;
+            if (checkBoxText != null) VerificationCheckBox.Text = checkBoxText;
+
             YesButton.Visible = yesButtonVisible;
             NoButton.Visible = noButtonVisible;
             Cancel_Button.Visible = cancelButtonVisible;
             VerificationCheckBox.Visible = checkBoxVisible;
             VerificationCheckBox.Checked = checkBoxChecked == true;
+
+            if (icon != MessageBoxIcon.None) ControlUtils.SetMessageBoxIcon(IconPictureBox, icon);
+
+            MessageLabel.Location = new Point(imageMarginX, MessageLabel.Location.Y);
+
+            #region Set default buttons
 
             CancelButton = cancelButtonVisible ? Cancel_Button : noButtonVisible ? NoButton : YesButton;
 
@@ -123,7 +121,11 @@ namespace AngelLoader.Forms
                     break;
             }
 
-            if (icon != MessageBoxIcon.None) ControlUtils.SetMessageBoxIcon(IconPictureBox, icon);
+            #endregion
+
+            #endregion
+
+            #region Size form
 
             int bottomBarContentWidth = ControlUtils.GetFlowLayoutPanelControlsWidthAll(BottomFLP);
             if (checkBoxVisible)
@@ -134,6 +136,7 @@ namespace AngelLoader.Forms
             MessageLabel.MaximumSize = new Size((width - imageMarginX) - 7, MessageLabel.MaximumSize.Height);
             int height = MessageLabel.Top + MessageLabel.Height + 15 + BottomFLP.Height;
 
+            // Add some padding between the bottom of the icon and the bottom panel
             if (icon != MessageBoxIcon.None && MessageLabel.Bottom <= IconPictureBox.Bottom + 10)
             {
                 height += 8;
@@ -141,6 +144,9 @@ namespace AngelLoader.Forms
 
             ClientSize = new Size(width, height);
 
+            #endregion
+
+            // If only one line of text, center the label vertically for a better look
             // +2 fudge factor because the label height and the font height are not exactly the same (height is
             // 1px less than font height in this case, but I don't know how consistent that is)
             if (MessageLabel.Height <= MessageLabel.Font.Height + 2)
@@ -153,11 +159,28 @@ namespace AngelLoader.Forms
 
             DialogResult = DialogResult.Cancel;
 
+            SetTheme(Config.VisualTheme);
+
             if (yesButtonVisible) YesButton.Click += YesButton_Click;
             if (noButtonVisible) NoButton.Click += NoButton_Click;
             if (cancelButtonVisible) Cancel_Button.Click += CancelButton_Click;
-            SetTheme(Config.VisualTheme);
         }
+
+        private void SetTheme(VisualTheme theme)
+        {
+            if (theme == VisualTheme.Dark)
+            {
+                ControlUtils.ChangeFormThemeMode(theme, this, _controlColors, x => x == BottomFLP || x == VerificationCheckBox);
+                BottomFLP.BackColor = DarkColors.Fen_DarkBackground;
+                VerificationCheckBox.DarkModeBackColor = DarkColors.Fen_DarkBackground;
+            }
+            else
+            {
+                VerificationCheckBox.BackColor = SystemColors.Control;
+            }
+        }
+
+        #region Button click event handlers
 
         private void YesButton_Click(object sender, EventArgs e)
         {
@@ -177,18 +200,6 @@ namespace AngelLoader.Forms
             Close();
         }
 
-        private void SetTheme(VisualTheme theme)
-        {
-            if (theme == VisualTheme.Dark)
-            {
-                ControlUtils.ChangeFormThemeMode(theme, this, _controlColors, x => x == BottomFLP || x == VerificationCheckBox);
-                BottomFLP.BackColor = DarkColors.Fen_DarkBackground;
-                VerificationCheckBox.DarkModeBackColor = DarkColors.Fen_DarkBackground;
-            }
-            else
-            {
-                VerificationCheckBox.BackColor = SystemColors.Control;
-            }
-        }
+        #endregion
     }
 }
--- AngelLoader/Forms/MessageBoxCustomForm.cs
+++ AngelLoader/Forms/MessageBoxCustomForm.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using AL_Common;
 using AngelLoader.DataClasses;
