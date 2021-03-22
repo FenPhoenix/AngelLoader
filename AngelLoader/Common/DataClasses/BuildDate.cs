931a5eca7fcefc57f3b9199618c56eb6c7688c96
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-03-21 18:24:43 -0700

    Fix broken build

--- AngelLoader/Common/DataClasses/BuildDate.cs
+++ AngelLoader/Common/DataClasses/BuildDate.cs
@@ -7,6 +7,6 @@ namespace AngelLoader.DataClasses
     [FenGenBuildDateDestClass]
     internal static class BuildDateSource
     {
-        internal const string BuildDate = "20210321234353";
+        internal const string BuildDate = "";
     }
 }
