f52e3b88bebbddf6baf028c7e91058f07bc3c448
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-03-22 19:29:01 -0700

    Preliminary progress bar darkening experimentation

---
+++ AngelLoader/Forms/ProgressBarRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngelLoader.WinAPI;
+using AngelLoader.WinAPI.Dialogs;
+
+namespace AngelLoader.Forms
+{
+    class ProgressBarRenderer
+    {
+        internal static IntPtr HTheme { get; private set; }
+
+        internal static void Reload()
+        {
+            Native.CloseThemeData(HTheme);
+            using var c = new Control();
+            HTheme = Native.OpenThemeData(c.Handle, "Progress");
+        }
+
+        public bool RenderBackground(IntPtr hdc, int partId, int stateId, Native.RECT pRect)
+        {
+            using var g = Graphics.FromHdc(hdc);
+
+            var rect = Rectangle.FromLTRB(pRect.left, pRect.top, pRect.right, pRect.bottom);
+
+            switch (partId)
+            {
+
+            }
+
+            return true;
+        }
+    }
+}
