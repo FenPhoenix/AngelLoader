636e0d2cd9c2d22835393d9cdbf13169bfbaed2f
FenPhoenix <FenPhoenix@users.noreply.github.com>
2021-12-16 21:17:27 -0800

    Fix tooltips weren't properly regenerated after DialogScope exit

--- AngelLoader/Forms/WinFormsNative/Win32ThemeHooks.cs
+++ AngelLoader/Forms/WinFormsNative/Win32ThemeHooks.cs
@@ -208,8 +208,10 @@ namespace AngelLoader.Forms.WinFormsNative
 
             public void Dispose()
             {
-                ControlUtils.RecreateAllToolTipHandles();
                 _disableHookedTheming = false;
+                // Do this AFTER re-enabling hooked theming,otherwise it doesn't take and we end up with
+                // dark-on-dark tooltips
+                ControlUtils.RecreateAllToolTipHandles();
                 var handles = Native.GetProcessWindowHandles();
                 foreach (IntPtr handle in handles)
                 {
