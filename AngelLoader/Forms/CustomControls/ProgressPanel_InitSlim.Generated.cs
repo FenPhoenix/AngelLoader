9ac36f406df234d269867c1d6dfb8f1eb588ffad
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-05-01 14:41:18 -0700

    Generate

--- AngelLoader/Forms/CustomControls/ProgressPanel_InitSlim.Generated.cs
+++ AngelLoader/Forms/CustomControls/ProgressPanel_InitSlim.Generated.cs
@@ -26,28 +26,28 @@
             this.ProgressCancelButton.MinimumSize = new System.Drawing.Size(88, 23);
             this.ProgressCancelButton.Padding = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.ProgressCancelButton.Size = new System.Drawing.Size(88, 23);
-            this.ProgressCancelButton.TabIndex = 4;
+            this.ProgressCancelButton.TabIndex = 7;
             this.ProgressCancelButton.UseVisualStyleBackColor = true;
             this.ProgressCancelButton.Click += new System.EventHandler(this.ProgressCancelButton_Click);
             // 
             // ProgressPercentLabel
             // 
-            this.ProgressPercentLabel.Location = new System.Drawing.Point(2, 40);
-            this.ProgressPercentLabel.Size = new System.Drawing.Size(418, 13);
+            this.ProgressPercentLabel.Location = new System.Drawing.Point(0, 40);
+            this.ProgressPercentLabel.Size = new System.Drawing.Size(424, 13);
             this.ProgressPercentLabel.TabIndex = 2;
             this.ProgressPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ProgressMessageLabel
             // 
-            this.ProgressMessageLabel.Location = new System.Drawing.Point(2, 8);
-            this.ProgressMessageLabel.Size = new System.Drawing.Size(418, 13);
+            this.ProgressMessageLabel.Location = new System.Drawing.Point(0, 8);
+            this.ProgressMessageLabel.Size = new System.Drawing.Size(424, 13);
             this.ProgressMessageLabel.TabIndex = 0;
             this.ProgressMessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // CurrentThingLabel
             // 
-            this.CurrentThingLabel.Location = new System.Drawing.Point(2, 24);
-            this.CurrentThingLabel.Size = new System.Drawing.Size(418, 13);
+            this.CurrentThingLabel.Location = new System.Drawing.Point(0, 24);
+            this.CurrentThingLabel.Size = new System.Drawing.Size(424, 13);
             this.CurrentThingLabel.TabIndex = 1;
             this.CurrentThingLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
@@ -61,20 +61,20 @@
             // 
             this.SubProgressBar.Location = new System.Drawing.Point(8, 120);
             this.SubProgressBar.Size = new System.Drawing.Size(408, 16);
-            this.SubProgressBar.TabIndex = 5;
+            this.SubProgressBar.TabIndex = 6;
             // 
             // CurrentSubThingLabel
             // 
             this.CurrentSubThingLabel.Location = new System.Drawing.Point(0, 88);
-            this.CurrentSubThingLabel.Size = new System.Drawing.Size(418, 13);
-            this.CurrentSubThingLabel.TabIndex = 1;
+            this.CurrentSubThingLabel.Size = new System.Drawing.Size(424, 13);
+            this.CurrentSubThingLabel.TabIndex = 4;
             this.CurrentSubThingLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // SubProgressPercentLabel
             // 
             this.SubProgressPercentLabel.Location = new System.Drawing.Point(0, 104);
-            this.SubProgressPercentLabel.Size = new System.Drawing.Size(418, 13);
-            this.SubProgressPercentLabel.TabIndex = 2;
+            this.SubProgressPercentLabel.Size = new System.Drawing.Size(424, 13);
+            this.SubProgressPercentLabel.TabIndex = 5;
             this.SubProgressPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ProgressPanel
