ecb366c5c433c89e27976b3762fa2352be4064b2
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-05-07 22:03:03 -0700

    Add perfect hash for langs (but not used or tested yet)

---
+++ AngelLoader/Common/LanguageSupport_PerfectHash.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace AngelLoader
+{
+    public static partial class LanguageSupport
+    {
+        /* ANSI-C code produced by gperf version 3.1 */
+        /* Command-line: gperf --output-file='C:\\gperf_out.txt' -t 'C:\\gperf_in.txt'  */
+        /* Computed positions: -k'1' */
+
+        //private const int TOTAL_KEYWORDS = 11;
+        private const int MIN_WORD_LENGTH = 5;
+        private const int MAX_WORD_LENGTH = 9;
+        //private const int MIN_HASH_VALUE = 5;
+        private const int MAX_HASH_VALUE = 22;
+        /* maximum key range = 18, duplicates = 0 */
+
+        private static readonly byte[] asso_values =
+        {
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 5,
+            0, 15, 10, 5, 0, 10, 0, 23, 23, 23,
+            23, 23, 0, 23, 5, 0, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+            23, 23, 23, 23, 23, 23
+        };
+
+        private sealed class LangNameAndEnumField
+        {
+            internal readonly string LangName;
+            internal readonly Language EnumField;
+            internal LangNameAndEnumField(string langName, Language enumField)
+            {
+                LangName = langName;
+                EnumField = enumField;
+            }
+        }
+
+        private static readonly LangNameAndEnumField?[] _perfectHash_LangList =
+        {
+            null, null, null, null, null,
+            new ("dutch", Language.Dutch),
+            new ("polish", Language.Polish),
+            new ("spanish", Language.Spanish),
+            new ("japanese", Language.Japanese),
+            new ("hungarian", Language.Hungarian),
+            new ("czech", Language.Czech),
+            new ("german", Language.German),
+            new ("russian", Language.Russian),
+            null, null, null,
+            new ("french", Language.French),
+            new ("italian", Language.Italian),
+            null, null, null, null,
+            new ("english", Language.English)
+        };
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint Hash(string str, int len) => ((uint)len) + asso_values[str[0]];
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool SeqEqual(string seq1, int seq1Start, int seq1Length, string seq2)
+        {
+            if (seq1Length != seq2.Length) return false;
+
+            for (int ci = seq1Start; ci < seq1Length; ci++)
+            {
+                if (seq1[ci] != seq2[ci]) return false;
+            }
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool TryGetValue(string str, int start, int end, [NotNullWhen(true)] out Language? result)
+        {
+            int len = end - start;
+            if (len is <= MAX_WORD_LENGTH and >= MIN_WORD_LENGTH)
+            {
+                uint key = Hash(str, len);
+
+                if (key <= MAX_HASH_VALUE)
+                {
+                    LangNameAndEnumField? language = _perfectHash_LangList[key];
+                    if (language == null)
+                    {
+                        result = null;
+                        return false;
+                    }
+
+                    if (SeqEqual(str, start, len, language.LangName))
+                    {
+                        result = language.EnumField;
+                        return true;
+                    }
+                }
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
