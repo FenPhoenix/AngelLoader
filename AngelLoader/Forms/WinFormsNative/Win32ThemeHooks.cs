3c8bb511e4345b73fc267fd728e358585bbe9d70
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-05-31 13:42:06 -0700

    correction cause I'm so exhausted from this crap that I misspoke

--- AngelLoader/Forms/WinFormsNative/Win32ThemeHooks.cs
+++ AngelLoader/Forms/WinFormsNative/Win32ThemeHooks.cs
@@ -14,7 +14,8 @@ namespace AngelLoader.Forms.WinFormsNative
     -MinHook.NET works on .NET 5+ in general, but the GetSysColor hook causes an ExecutionEngineException. Even
      if I have it literally just return 0. It DOES start executing the hooked method (confirmed by putting a
      Trace.WriteLine() in there), but crashes with the exception on return.
-    -MinSharp (a wrapper, not a port like MinHook.NET) also has this same issue.
+    -MinSharp (a wrapper, not a port like MinHook.NET) doesn't even get as far as loading its native dll.
+     Ever, not matter what fiddling I do. So anyway.
     No exceptions for any hook library on Framework.
     God only knows what the god damn hell the .NET version has to do with running A NATIVE WINDOWS PROC but hey.
     -Only thing I can think of is using native C++ hook code, shoving it in a dll, and p/invoking it like "hey
