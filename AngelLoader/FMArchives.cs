467594e2861793639682137e384f4e77e23bf227
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-06-18 19:32:01 -0700

    Remove a blank line

--- AngelLoader/FMArchives.cs
+++ AngelLoader/FMArchives.cs
@@ -175,7 +175,6 @@ namespace AngelLoader
 
                 if (!singleArchivePath)
                 {
-
                     (bool accepted, List<string> selectedItems) = Core.Dialogs.ShowListDialog(
                         messageTop:
                         (singleArchive
