9ef090ed82638b44130eb916299fc22afb0b8845
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-06-26 21:25:34 -0700

    Cleanup unused usings

--- AngelLoader/Forms/OriginalGameModsForm.cs
+++ AngelLoader/Forms/OriginalGameModsForm.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
-using AngelLoader.DataClasses;
 using AngelLoader.Forms.CustomControls;
 using static AL_Common.Common;
 using static AngelLoader.GameSupport;
