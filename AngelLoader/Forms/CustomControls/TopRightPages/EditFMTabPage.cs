c53c54e559ab5515f01ffc57008134764e3c279c
FenPhoenix <FenPhoenix@users.noreply.github.com>
2022-11-23 14:55:40 -0800

    Note

--- AngelLoader/Forms/CustomControls/TopRightPages/EditFMTabPage.cs
+++ AngelLoader/Forms/CustomControls/TopRightPages/EditFMTabPage.cs
@@ -18,6 +18,7 @@ namespace AngelLoader.Forms.CustomControls
     @vNext: Focus tab pages after construct, because a control in the page gets focus
     @vNext: Use _constructed check instead of _page != null check, so _page doesn't need ! after it all the time
     @vNext: Test lazy-loaded lang error functionality
+    @vNext: Mark any new business logic that's been moved back into the view (@VBL) for later review
     */
 
     public sealed class EditFMTabPage : Lazy_TabsBase
