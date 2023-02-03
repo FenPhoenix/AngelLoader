07a05be815e2f7b1b727a26f2845cb6892b1c34b
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-02-03 03:31:17 -0800

    Tab order on TB page

--- AngelLoader/Forms/Settings/ThiefBuddyPage.Designer.cs
+++ AngelLoader/Forms/Settings/ThiefBuddyPage.Designer.cs
@@ -32,8 +32,6 @@ partial class ThiefBuddyPage
     private void InitializeComponent()
     {
             this.PagePanel = new System.Windows.Forms.Panel();
-            this.DummyAutoScrollPanel = new System.Windows.Forms.Control();
-            this.TBHelpPictureBox = new System.Windows.Forms.PictureBox();
             this.ThiefBuddyOptionsGroupBox = new AngelLoader.Forms.CustomControls.DarkGroupBox();
             this.HorizDiv1 = new AngelLoader.Forms.CustomControls.DarkHorizontalDivider();
             this.TBInstallStatusLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
@@ -43,11 +41,13 @@ partial class ThiefBuddyPage
             this.RunTBNeverRadioButton = new AngelLoader.Forms.CustomControls.DarkRadioButton();
             this.RunTBAskRadioButton = new AngelLoader.Forms.CustomControls.DarkRadioButton();
             this.GetTBLinkLabel = new AngelLoader.Forms.CustomControls.DarkLinkLabel();
+            this.DummyAutoScrollPanel = new System.Windows.Forms.Control();
+            this.TBHelpPictureBox = new System.Windows.Forms.PictureBox();
             this.WhatIsTBHelpLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
             this.PagePanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).BeginInit();
             this.ThiefBuddyOptionsGroupBox.SuspendLayout();
             this.RunTBPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // PagePanel
@@ -62,7 +62,104 @@ partial class ThiefBuddyPage
             this.PagePanel.Location = new System.Drawing.Point(0, 0);
             this.PagePanel.Name = "PagePanel";
             this.PagePanel.Size = new System.Drawing.Size(440, 692);
-            this.PagePanel.TabIndex = 1;
+            this.PagePanel.TabIndex = 0;
+            // 
+            // ThiefBuddyOptionsGroupBox
+            // 
+            this.ThiefBuddyOptionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.HorizDiv1);
+            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.TBInstallStatusLabel);
+            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.RunTBPanel);
+            this.ThiefBuddyOptionsGroupBox.Location = new System.Drawing.Point(8, 8);
+            this.ThiefBuddyOptionsGroupBox.MinimumSize = new System.Drawing.Size(424, 0);
+            this.ThiefBuddyOptionsGroupBox.Name = "ThiefBuddyOptionsGroupBox";
+            this.ThiefBuddyOptionsGroupBox.Size = new System.Drawing.Size(424, 168);
+            this.ThiefBuddyOptionsGroupBox.TabIndex = 0;
+            this.ThiefBuddyOptionsGroupBox.TabStop = false;
+            this.ThiefBuddyOptionsGroupBox.Text = "Thief Buddy options";
+            // 
+            // HorizDiv1
+            // 
+            this.HorizDiv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HorizDiv1.Location = new System.Drawing.Point(16, 40);
+            this.HorizDiv1.Name = "HorizDiv1";
+            this.HorizDiv1.Size = new System.Drawing.Size(392, 16);
+            this.HorizDiv1.TabIndex = 1;
+            // 
+            // TBInstallStatusLabel
+            // 
+            this.TBInstallStatusLabel.AutoSize = true;
+            this.TBInstallStatusLabel.Location = new System.Drawing.Point(16, 24);
+            this.TBInstallStatusLabel.Name = "TBInstallStatusLabel";
+            this.TBInstallStatusLabel.Size = new System.Drawing.Size(136, 13);
+            this.TBInstallStatusLabel.TabIndex = 0;
+            this.TBInstallStatusLabel.Text = "Thief Buddy is not installed.";
+            // 
+            // RunTBPanel
+            // 
+            this.RunTBPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RunTBPanel.Controls.Add(this.RunThiefBuddyWhenPlayingFMsLabel);
+            this.RunTBPanel.Controls.Add(this.RunTBAlwaysRadioButton);
+            this.RunTBPanel.Controls.Add(this.RunTBNeverRadioButton);
+            this.RunTBPanel.Controls.Add(this.RunTBAskRadioButton);
+            this.RunTBPanel.Location = new System.Drawing.Point(16, 60);
+            this.RunTBPanel.Name = "RunTBPanel";
+            this.RunTBPanel.Size = new System.Drawing.Size(392, 100);
+            this.RunTBPanel.TabIndex = 2;
+            // 
+            // RunThiefBuddyWhenPlayingFMsLabel
+            // 
+            this.RunThiefBuddyWhenPlayingFMsLabel.AutoSize = true;
+            this.RunThiefBuddyWhenPlayingFMsLabel.Location = new System.Drawing.Point(0, 0);
+            this.RunThiefBuddyWhenPlayingFMsLabel.Name = "RunThiefBuddyWhenPlayingFMsLabel";
+            this.RunThiefBuddyWhenPlayingFMsLabel.Size = new System.Drawing.Size(178, 13);
+            this.RunThiefBuddyWhenPlayingFMsLabel.TabIndex = 0;
+            this.RunThiefBuddyWhenPlayingFMsLabel.Text = "Run Thief Buddy when playing FMs:";
+            // 
+            // RunTBAlwaysRadioButton
+            // 
+            this.RunTBAlwaysRadioButton.AutoSize = true;
+            this.RunTBAlwaysRadioButton.Location = new System.Drawing.Point(8, 24);
+            this.RunTBAlwaysRadioButton.Name = "RunTBAlwaysRadioButton";
+            this.RunTBAlwaysRadioButton.Size = new System.Drawing.Size(58, 17);
+            this.RunTBAlwaysRadioButton.TabIndex = 1;
+            this.RunTBAlwaysRadioButton.Text = "Always";
+            this.RunTBAlwaysRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // RunTBNeverRadioButton
+            // 
+            this.RunTBNeverRadioButton.AutoSize = true;
+            this.RunTBNeverRadioButton.Location = new System.Drawing.Point(8, 72);
+            this.RunTBNeverRadioButton.Name = "RunTBNeverRadioButton";
+            this.RunTBNeverRadioButton.Size = new System.Drawing.Size(54, 17);
+            this.RunTBNeverRadioButton.TabIndex = 3;
+            this.RunTBNeverRadioButton.Text = "Never";
+            this.RunTBNeverRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // RunTBAskRadioButton
+            // 
+            this.RunTBAskRadioButton.AutoSize = true;
+            this.RunTBAskRadioButton.Checked = true;
+            this.RunTBAskRadioButton.Location = new System.Drawing.Point(8, 48);
+            this.RunTBAskRadioButton.Name = "RunTBAskRadioButton";
+            this.RunTBAskRadioButton.Size = new System.Drawing.Size(94, 17);
+            this.RunTBAskRadioButton.TabIndex = 2;
+            this.RunTBAskRadioButton.TabStop = true;
+            this.RunTBAskRadioButton.Text = "Ask every time";
+            this.RunTBAskRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // GetTBLinkLabel
+            // 
+            this.GetTBLinkLabel.AutoSize = true;
+            this.GetTBLinkLabel.Location = new System.Drawing.Point(27, 208);
+            this.GetTBLinkLabel.Name = "GetTBLinkLabel";
+            this.GetTBLinkLabel.Size = new System.Drawing.Size(84, 13);
+            this.GetTBLinkLabel.TabIndex = 2;
+            this.GetTBLinkLabel.TabStop = true;
+            this.GetTBLinkLabel.Text = "Get Thief Buddy";
             // 
             // DummyAutoScrollPanel
             // 
@@ -81,103 +178,6 @@ partial class ThiefBuddyPage
             this.TBHelpPictureBox.TabIndex = 9;
             this.TBHelpPictureBox.TabStop = false;
             // 
-            // ThiefBuddyOptionsGroupBox
-            // 
-            this.ThiefBuddyOptionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.HorizDiv1);
-            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.TBInstallStatusLabel);
-            this.ThiefBuddyOptionsGroupBox.Controls.Add(this.RunTBPanel);
-            this.ThiefBuddyOptionsGroupBox.Location = new System.Drawing.Point(8, 8);
-            this.ThiefBuddyOptionsGroupBox.MinimumSize = new System.Drawing.Size(424, 0);
-            this.ThiefBuddyOptionsGroupBox.Name = "ThiefBuddyOptionsGroupBox";
-            this.ThiefBuddyOptionsGroupBox.Size = new System.Drawing.Size(424, 168);
-            this.ThiefBuddyOptionsGroupBox.TabIndex = 13;
-            this.ThiefBuddyOptionsGroupBox.TabStop = false;
-            this.ThiefBuddyOptionsGroupBox.Text = "Thief Buddy options";
-            // 
-            // HorizDiv1
-            // 
-            this.HorizDiv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.HorizDiv1.Location = new System.Drawing.Point(16, 40);
-            this.HorizDiv1.Name = "HorizDiv1";
-            this.HorizDiv1.Size = new System.Drawing.Size(392, 16);
-            this.HorizDiv1.TabIndex = 7;
-            // 
-            // TBInstallStatusLabel
-            // 
-            this.TBInstallStatusLabel.AutoSize = true;
-            this.TBInstallStatusLabel.Location = new System.Drawing.Point(16, 24);
-            this.TBInstallStatusLabel.Name = "TBInstallStatusLabel";
-            this.TBInstallStatusLabel.Size = new System.Drawing.Size(136, 13);
-            this.TBInstallStatusLabel.TabIndex = 6;
-            this.TBInstallStatusLabel.Text = "Thief Buddy is not installed.";
-            // 
-            // RunTBPanel
-            // 
-            this.RunTBPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.RunTBPanel.Controls.Add(this.RunThiefBuddyWhenPlayingFMsLabel);
-            this.RunTBPanel.Controls.Add(this.RunTBAlwaysRadioButton);
-            this.RunTBPanel.Controls.Add(this.RunTBNeverRadioButton);
-            this.RunTBPanel.Controls.Add(this.RunTBAskRadioButton);
-            this.RunTBPanel.Location = new System.Drawing.Point(16, 60);
-            this.RunTBPanel.Name = "RunTBPanel";
-            this.RunTBPanel.Size = new System.Drawing.Size(392, 100);
-            this.RunTBPanel.TabIndex = 5;
-            // 
-            // RunThiefBuddyWhenPlayingFMsLabel
-            // 
-            this.RunThiefBuddyWhenPlayingFMsLabel.AutoSize = true;
-            this.RunThiefBuddyWhenPlayingFMsLabel.Location = new System.Drawing.Point(0, 0);
-            this.RunThiefBuddyWhenPlayingFMsLabel.Name = "RunThiefBuddyWhenPlayingFMsLabel";
-            this.RunThiefBuddyWhenPlayingFMsLabel.Size = new System.Drawing.Size(178, 13);
-            this.RunThiefBuddyWhenPlayingFMsLabel.TabIndex = 5;
-            this.RunThiefBuddyWhenPlayingFMsLabel.Text = "Run Thief Buddy when playing FMs:";
-            // 
-            // RunTBAlwaysRadioButton
-            // 
-            this.RunTBAlwaysRadioButton.AutoSize = true;
-            this.RunTBAlwaysRadioButton.Location = new System.Drawing.Point(8, 24);
-            this.RunTBAlwaysRadioButton.Name = "RunTBAlwaysRadioButton";
-            this.RunTBAlwaysRadioButton.Size = new System.Drawing.Size(58, 17);
-            this.RunTBAlwaysRadioButton.TabIndex = 4;
-            this.RunTBAlwaysRadioButton.Text = "Always";
-            this.RunTBAlwaysRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // RunTBNeverRadioButton
-            // 
-            this.RunTBNeverRadioButton.AutoSize = true;
-            this.RunTBNeverRadioButton.Location = new System.Drawing.Point(8, 72);
-            this.RunTBNeverRadioButton.Name = "RunTBNeverRadioButton";
-            this.RunTBNeverRadioButton.Size = new System.Drawing.Size(54, 17);
-            this.RunTBNeverRadioButton.TabIndex = 4;
-            this.RunTBNeverRadioButton.Text = "Never";
-            this.RunTBNeverRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // RunTBAskRadioButton
-            // 
-            this.RunTBAskRadioButton.AutoSize = true;
-            this.RunTBAskRadioButton.Checked = true;
-            this.RunTBAskRadioButton.Location = new System.Drawing.Point(8, 48);
-            this.RunTBAskRadioButton.Name = "RunTBAskRadioButton";
-            this.RunTBAskRadioButton.Size = new System.Drawing.Size(94, 17);
-            this.RunTBAskRadioButton.TabIndex = 4;
-            this.RunTBAskRadioButton.TabStop = true;
-            this.RunTBAskRadioButton.Text = "Ask every time";
-            this.RunTBAskRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // GetTBLinkLabel
-            // 
-            this.GetTBLinkLabel.AutoSize = true;
-            this.GetTBLinkLabel.Location = new System.Drawing.Point(27, 208);
-            this.GetTBLinkLabel.Name = "GetTBLinkLabel";
-            this.GetTBLinkLabel.Size = new System.Drawing.Size(84, 13);
-            this.GetTBLinkLabel.TabIndex = 10;
-            this.GetTBLinkLabel.TabStop = true;
-            this.GetTBLinkLabel.Text = "Get Thief Buddy";
-            // 
             // WhatIsTBHelpLabel
             // 
             this.WhatIsTBHelpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -186,7 +186,7 @@ partial class ThiefBuddyPage
             this.WhatIsTBHelpLabel.MinimumSize = new System.Drawing.Size(0, 13);
             this.WhatIsTBHelpLabel.Name = "WhatIsTBHelpLabel";
             this.WhatIsTBHelpLabel.Size = new System.Drawing.Size(389, 28);
-            this.WhatIsTBHelpLabel.TabIndex = 8;
+            this.WhatIsTBHelpLabel.TabIndex = 1;
             this.WhatIsTBHelpLabel.Text = "Thief Buddy, by VoiceActor, is a tool that allows you to make and restore multipl" +
     "e quicksaves.";
             this.WhatIsTBHelpLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -200,11 +200,11 @@ partial class ThiefBuddyPage
             this.Size = new System.Drawing.Size(440, 692);
             this.PagePanel.ResumeLayout(false);
             this.PagePanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).EndInit();
             this.ThiefBuddyOptionsGroupBox.ResumeLayout(false);
             this.ThiefBuddyOptionsGroupBox.PerformLayout();
             this.RunTBPanel.ResumeLayout(false);
             this.RunTBPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).EndInit();
             this.ResumeLayout(false);
 
     }
--- AngelLoader/Forms/Settings/ThiefBuddyPage_InitSlim.Generated.cs
+++ AngelLoader/Forms/Settings/ThiefBuddyPage_InitSlim.Generated.cs
@@ -8,8 +8,6 @@ partial class ThiefBuddyPage
     private void InitSlim()
     {
         this.PagePanel = new System.Windows.Forms.Panel();
-        this.DummyAutoScrollPanel = new System.Windows.Forms.Control();
-        this.TBHelpPictureBox = new System.Windows.Forms.PictureBox();
         this.ThiefBuddyOptionsGroupBox = new AngelLoader.Forms.CustomControls.DarkGroupBox();
         this.HorizDiv1 = new AngelLoader.Forms.CustomControls.DarkHorizontalDivider();
         this.TBInstallStatusLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
@@ -19,11 +17,13 @@ partial class ThiefBuddyPage
         this.RunTBNeverRadioButton = new AngelLoader.Forms.CustomControls.DarkRadioButton();
         this.RunTBAskRadioButton = new AngelLoader.Forms.CustomControls.DarkRadioButton();
         this.GetTBLinkLabel = new AngelLoader.Forms.CustomControls.DarkLinkLabel();
+        this.DummyAutoScrollPanel = new System.Windows.Forms.Control();
+        this.TBHelpPictureBox = new System.Windows.Forms.PictureBox();
         this.WhatIsTBHelpLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
         this.PagePanel.SuspendLayout();
-        ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).BeginInit();
         this.ThiefBuddyOptionsGroupBox.SuspendLayout();
         this.RunTBPanel.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).BeginInit();
         this.SuspendLayout();
         // 
         // PagePanel
@@ -36,7 +36,81 @@ partial class ThiefBuddyPage
         this.PagePanel.Controls.Add(this.WhatIsTBHelpLabel);
         this.PagePanel.Dock = System.Windows.Forms.DockStyle.Fill;
         this.PagePanel.Size = new System.Drawing.Size(440, 692);
-        this.PagePanel.TabIndex = 1;
+        this.PagePanel.TabIndex = 0;
+        // 
+        // ThiefBuddyOptionsGroupBox
+        // 
+        this.ThiefBuddyOptionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
+        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.HorizDiv1);
+        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.TBInstallStatusLabel);
+        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.RunTBPanel);
+        this.ThiefBuddyOptionsGroupBox.Location = new System.Drawing.Point(8, 8);
+        this.ThiefBuddyOptionsGroupBox.MinimumSize = new System.Drawing.Size(424, 0);
+        this.ThiefBuddyOptionsGroupBox.Size = new System.Drawing.Size(424, 168);
+        this.ThiefBuddyOptionsGroupBox.TabIndex = 0;
+        this.ThiefBuddyOptionsGroupBox.TabStop = false;
+        // 
+        // HorizDiv1
+        // 
+        this.HorizDiv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
+        this.HorizDiv1.Location = new System.Drawing.Point(16, 40);
+        this.HorizDiv1.Size = new System.Drawing.Size(392, 16);
+        this.HorizDiv1.TabIndex = 1;
+        // 
+        // TBInstallStatusLabel
+        // 
+        this.TBInstallStatusLabel.AutoSize = true;
+        this.TBInstallStatusLabel.Location = new System.Drawing.Point(16, 24);
+        this.TBInstallStatusLabel.TabIndex = 0;
+        // 
+        // RunTBPanel
+        // 
+        this.RunTBPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
+        this.RunTBPanel.Controls.Add(this.RunThiefBuddyWhenPlayingFMsLabel);
+        this.RunTBPanel.Controls.Add(this.RunTBAlwaysRadioButton);
+        this.RunTBPanel.Controls.Add(this.RunTBNeverRadioButton);
+        this.RunTBPanel.Controls.Add(this.RunTBAskRadioButton);
+        this.RunTBPanel.Location = new System.Drawing.Point(16, 60);
+        this.RunTBPanel.Size = new System.Drawing.Size(392, 100);
+        this.RunTBPanel.TabIndex = 2;
+        // 
+        // RunThiefBuddyWhenPlayingFMsLabel
+        // 
+        this.RunThiefBuddyWhenPlayingFMsLabel.AutoSize = true;
+        this.RunThiefBuddyWhenPlayingFMsLabel.TabIndex = 0;
+        // 
+        // RunTBAlwaysRadioButton
+        // 
+        this.RunTBAlwaysRadioButton.AutoSize = true;
+        this.RunTBAlwaysRadioButton.Location = new System.Drawing.Point(8, 24);
+        this.RunTBAlwaysRadioButton.TabIndex = 1;
+        this.RunTBAlwaysRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // RunTBNeverRadioButton
+        // 
+        this.RunTBNeverRadioButton.AutoSize = true;
+        this.RunTBNeverRadioButton.Location = new System.Drawing.Point(8, 72);
+        this.RunTBNeverRadioButton.TabIndex = 3;
+        this.RunTBNeverRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // RunTBAskRadioButton
+        // 
+        this.RunTBAskRadioButton.AutoSize = true;
+        this.RunTBAskRadioButton.Checked = true;
+        this.RunTBAskRadioButton.Location = new System.Drawing.Point(8, 48);
+        this.RunTBAskRadioButton.TabIndex = 2;
+        this.RunTBAskRadioButton.TabStop = true;
+        this.RunTBAskRadioButton.UseVisualStyleBackColor = true;
+        // 
+        // GetTBLinkLabel
+        // 
+        this.GetTBLinkLabel.AutoSize = true;
+        this.GetTBLinkLabel.Location = new System.Drawing.Point(27, 208);
+        this.GetTBLinkLabel.TabIndex = 2;
+        this.GetTBLinkLabel.TabStop = true;
         // 
         // DummyAutoScrollPanel
         // 
@@ -53,80 +127,6 @@ partial class ThiefBuddyPage
         this.TBHelpPictureBox.TabIndex = 9;
         this.TBHelpPictureBox.TabStop = false;
         // 
-        // ThiefBuddyOptionsGroupBox
-        // 
-        this.ThiefBuddyOptionsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.HorizDiv1);
-        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.TBInstallStatusLabel);
-        this.ThiefBuddyOptionsGroupBox.Controls.Add(this.RunTBPanel);
-        this.ThiefBuddyOptionsGroupBox.Location = new System.Drawing.Point(8, 8);
-        this.ThiefBuddyOptionsGroupBox.MinimumSize = new System.Drawing.Size(424, 0);
-        this.ThiefBuddyOptionsGroupBox.Size = new System.Drawing.Size(424, 168);
-        this.ThiefBuddyOptionsGroupBox.TabIndex = 13;
-        this.ThiefBuddyOptionsGroupBox.TabStop = false;
-        // 
-        // HorizDiv1
-        // 
-        this.HorizDiv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.HorizDiv1.Location = new System.Drawing.Point(16, 40);
-        this.HorizDiv1.Size = new System.Drawing.Size(392, 16);
-        this.HorizDiv1.TabIndex = 7;
-        // 
-        // TBInstallStatusLabel
-        // 
-        this.TBInstallStatusLabel.AutoSize = true;
-        this.TBInstallStatusLabel.Location = new System.Drawing.Point(16, 24);
-        this.TBInstallStatusLabel.TabIndex = 6;
-        // 
-        // RunTBPanel
-        // 
-        this.RunTBPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.RunTBPanel.Controls.Add(this.RunThiefBuddyWhenPlayingFMsLabel);
-        this.RunTBPanel.Controls.Add(this.RunTBAlwaysRadioButton);
-        this.RunTBPanel.Controls.Add(this.RunTBNeverRadioButton);
-        this.RunTBPanel.Controls.Add(this.RunTBAskRadioButton);
-        this.RunTBPanel.Location = new System.Drawing.Point(16, 60);
-        this.RunTBPanel.Size = new System.Drawing.Size(392, 100);
-        this.RunTBPanel.TabIndex = 5;
-        // 
-        // RunThiefBuddyWhenPlayingFMsLabel
-        // 
-        this.RunThiefBuddyWhenPlayingFMsLabel.AutoSize = true;
-        this.RunThiefBuddyWhenPlayingFMsLabel.TabIndex = 5;
-        // 
-        // RunTBAlwaysRadioButton
-        // 
-        this.RunTBAlwaysRadioButton.AutoSize = true;
-        this.RunTBAlwaysRadioButton.Location = new System.Drawing.Point(8, 24);
-        this.RunTBAlwaysRadioButton.TabIndex = 4;
-        this.RunTBAlwaysRadioButton.UseVisualStyleBackColor = true;
-        // 
-        // RunTBNeverRadioButton
-        // 
-        this.RunTBNeverRadioButton.AutoSize = true;
-        this.RunTBNeverRadioButton.Location = new System.Drawing.Point(8, 72);
-        this.RunTBNeverRadioButton.TabIndex = 4;
-        this.RunTBNeverRadioButton.UseVisualStyleBackColor = true;
-        // 
-        // RunTBAskRadioButton
-        // 
-        this.RunTBAskRadioButton.AutoSize = true;
-        this.RunTBAskRadioButton.Checked = true;
-        this.RunTBAskRadioButton.Location = new System.Drawing.Point(8, 48);
-        this.RunTBAskRadioButton.TabIndex = 4;
-        this.RunTBAskRadioButton.TabStop = true;
-        this.RunTBAskRadioButton.UseVisualStyleBackColor = true;
-        // 
-        // GetTBLinkLabel
-        // 
-        this.GetTBLinkLabel.AutoSize = true;
-        this.GetTBLinkLabel.Location = new System.Drawing.Point(27, 208);
-        this.GetTBLinkLabel.TabIndex = 10;
-        this.GetTBLinkLabel.TabStop = true;
-        // 
         // WhatIsTBHelpLabel
         // 
         this.WhatIsTBHelpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
@@ -134,7 +134,7 @@ partial class ThiefBuddyPage
         this.WhatIsTBHelpLabel.Location = new System.Drawing.Point(27, 180);
         this.WhatIsTBHelpLabel.MinimumSize = new System.Drawing.Size(0, 13);
         this.WhatIsTBHelpLabel.Size = new System.Drawing.Size(389, 28);
-        this.WhatIsTBHelpLabel.TabIndex = 8;
+        this.WhatIsTBHelpLabel.TabIndex = 1;
         this.WhatIsTBHelpLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
         // 
         // ThiefBuddyPage
@@ -145,11 +145,11 @@ partial class ThiefBuddyPage
         this.Size = new System.Drawing.Size(440, 692);
         this.PagePanel.ResumeLayout(false);
         this.PagePanel.PerformLayout();
-        ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).EndInit();
         this.ThiefBuddyOptionsGroupBox.ResumeLayout(false);
         this.ThiefBuddyOptionsGroupBox.PerformLayout();
         this.RunTBPanel.ResumeLayout(false);
         this.RunTBPanel.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize)(this.TBHelpPictureBox)).EndInit();
         this.ResumeLayout(false);
     }
 }
