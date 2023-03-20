2913de5dd784db0408f776408c8b0a8d1c2b011e
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-03-20 03:21:31 -0700

    Cleanup

--- FMScanner/Core/RtfToTextConverter.cs
+++ FMScanner/Core/RtfToTextConverter.cs
@@ -233,52 +233,51 @@ public sealed class RtfToTextConverter : AL_Common.RTFParserBase
 
     #region Font to Unicode conversion tables
 
-    private readonly Dictionary<int, int>
-        _charSetToCodePage = new()
-        {
-            { 0, _windows1252 },    // "ANSI" (1252)
+    private readonly Dictionary<int, int> _charSetToCodePage = new()
+    {
+        { 0, _windows1252 },   // "ANSI" (1252)
 
-            // TODO: Code page 0 ("Default") is variable... should we force it to 1252?
-            // "The system default Windows ANSI code page" says the doc page.
-            // Terrible. Fortunately only two known FMs define it in a font entry, and neither one actually uses
-            // said font entry. Still, maybe this should be 1252 as well, since we're rolling dice anyway we may
-            // as well go with the statistically likeliest?
-            { 1, 0 },               // Default
+        // TODO: Code page 0 ("Default") is variable... should we force it to 1252?
+        // "The system default Windows ANSI code page" says the doc page.
+        // Terrible. Fortunately only two known FMs define it in a font entry, and neither one actually uses
+        // said font entry. Still, maybe this should be 1252 as well, since we're rolling dice anyway we may
+        // as well go with the statistically likeliest?
+        { 1, 0 },              // Default
 
-            { 2, 42 },              // Symbol
-            { 77, 10000 },          // Mac Roman
-            { 78, 10001 },          // Mac Shift Jis
-            { 79, 10003 },          // Mac Hangul
-            { 80, 10008 },          // Mac GB2312
-            { 81, 10002 },          // Mac Big5
-            //82                    // Mac Johab (old)
-            { 83, 10005 },          // Mac Hebrew
-            { 84, 10004 },          // Mac Arabic
-            { 85, 10006 },          // Mac Greek
-            { 86, 10081 },          // Mac Turkish
-            { 87, 10021 },          // Mac Thai
-            { 88, 10029 },          // Mac East Europe
-            { 89, 10007 },          // Mac Russian
-            { 128, _shiftJisWin },  // Shift JIS (Windows-31J) (932)
-            { 129, 949 },           // Hangul
-            { 130, 1361 },          // Johab
-            { 134, 936 },           // GB2312
-            { 136, 950 },           // Big5
-            { 161, 1253 },          // Greek
-            { 162, 1254 },          // Turkish
-            { 163, 1258 },          // Vietnamese
-            { 177, 1255 },          // Hebrew
-            { 178, 1256 },          // Arabic
-            //179                   // Arabic Traditional (old)
-            //180                   // Arabic user (old)
-            //181                   // Hebrew user (old)
-            { 186, 1257 },          // Baltic
-            { 204, 1251 },          // Russian
-            { 222, 874 },           // Thai
-            { 238, 1250 },          // Eastern European
-            { 254, 437 },           // PC 437
-            { 255, 850 }            // OEM
-        };
+        { 2, 42 },             // Symbol
+        { 77, 10000 },         // Mac Roman
+        { 78, 10001 },         // Mac Shift Jis
+        { 79, 10003 },         // Mac Hangul
+        { 80, 10008 },         // Mac GB2312
+        { 81, 10002 },         // Mac Big5
+        //82                   // Mac Johab (old)
+        { 83, 10005 },         // Mac Hebrew
+        { 84, 10004 },         // Mac Arabic
+        { 85, 10006 },         // Mac Greek
+        { 86, 10081 },         // Mac Turkish
+        { 87, 10021 },         // Mac Thai
+        { 88, 10029 },         // Mac East Europe
+        { 89, 10007 },         // Mac Russian
+        { 128, _shiftJisWin }, // Shift JIS (Windows-31J) (932)
+        { 129, 949 },          // Hangul
+        { 130, 1361 },         // Johab
+        { 134, 936 },          // GB2312
+        { 136, 950 },          // Big5
+        { 161, 1253 },         // Greek
+        { 162, 1254 },         // Turkish
+        { 163, 1258 },         // Vietnamese
+        { 177, 1255 },         // Hebrew
+        { 178, 1256 },         // Arabic
+        //179                  // Arabic Traditional (old)
+        //180                  // Arabic user (old)
+        //181                  // Hebrew user (old)
+        { 186, 1257 },         // Baltic
+        { 204, 1251 },         // Russian
+        { 222, 874 },          // Thai
+        { 238, 1250 },         // Eastern European
+        { 254, 437 },          // PC 437
+        { 255, 850 }           // OEM
+    };
 
     /*
     Many RTF files put emoji-like glyphs into text not with a Unicode character, but by just putting in a
