5caf71d2c8f1729b77174574c48573127b44f40b
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-03-26 04:10:05 -0700

    Remove even more unnecessary code

--- SharpCompress/IO/BufferedSubStream.cs
+++ SharpCompress/IO/BufferedSubStream.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
+using SharpCompress.Common;
 
 namespace SharpCompress.IO;
 
-internal sealed class BufferedSubStream : NonDisposingStream
+internal sealed class BufferedSubStream : Stream
 {
     private long _position;
     private int _cacheOffset;
@@ -11,14 +12,16 @@ internal sealed class BufferedSubStream : NonDisposingStream
     // @SharpCompress: Crappy static array for now, until we can figure out how to pass it in for the batch scan
     private static readonly byte[] _cache = new byte[32 << 10];
 
-    public BufferedSubStream(Stream stream, long origin, long bytesToRead)
-        : base(stream, throwOnDispose: false)
-    {
-        _position = origin;
-        BytesLeftToRead = bytesToRead;
-    }
+    private long _bytesLeftToRead;
+    private readonly Stream _stream;
 
-    private long BytesLeftToRead { get; set; }
+    public BufferedSubStream(Stream stream, long origin, long bytesToRead)
+    {
+        _stream = stream;
+
+        _position = origin;
+        _bytesLeftToRead = bytesToRead;
+    }
 
     public override bool CanRead => true;
 
@@ -28,7 +31,7 @@ internal sealed class BufferedSubStream : NonDisposingStream
 
     public override void Flush() => throw new NotSupportedException();
 
-    public override long Length => BytesLeftToRead;
+    public override long Length => _bytesLeftToRead;
 
     public override long Position
     {
@@ -36,11 +39,17 @@ internal sealed class BufferedSubStream : NonDisposingStream
         set => throw new NotSupportedException();
     }
 
+    // FenPhoenix 2023: avoid a zillion byte[1] allocations
+    public override int ReadByte()
+    {
+        return Read(FEN_COMMON.Byte1, 0, 1) == 0 ? -1 : (int)FEN_COMMON.Byte1[0];
+    }
+
     public override int Read(byte[] buffer, int offset, int count)
     {
-        if (count > BytesLeftToRead)
+        if (count > _bytesLeftToRead)
         {
-            count = (int)BytesLeftToRead;
+            count = (int)_bytesLeftToRead;
         }
 
         if (count > 0)
@@ -48,8 +57,8 @@ internal sealed class BufferedSubStream : NonDisposingStream
             if (_cacheLength == 0)
             {
                 _cacheOffset = 0;
-                Stream.Position = _position;
-                _cacheLength = Stream.Read(_cache, 0, _cache.Length);
+                _stream.Position = _position;
+                _cacheLength = _stream.Read(_cache, 0, _cache.Length);
                 _position += _cacheLength;
             }
 
@@ -61,7 +70,7 @@ internal sealed class BufferedSubStream : NonDisposingStream
             Buffer.BlockCopy(_cache, _cacheOffset, buffer, offset, count);
             _cacheOffset += count;
             _cacheLength -= count;
-            BytesLeftToRead -= count;
+            _bytesLeftToRead -= count;
         }
 
         return count;
@@ -72,4 +81,12 @@ internal sealed class BufferedSubStream : NonDisposingStream
     public override void SetLength(long value) => throw new NotSupportedException();
 
     public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+#if !NETFRAMEWORK && !NETSTANDARD2_0
+
+    public override int Read(Span<byte> buffer) => Stream.Read(buffer);
+
+    public override void Write(ReadOnlySpan<byte> buffer) => throw new NotSupportedException();
+
+#endif
 }
