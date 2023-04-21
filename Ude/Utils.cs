7ea52a44561c7aa734ea41b6be8582008995c502
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-04-21 03:14:10 -0700

    Compress some byte arrays in Ude
    Decompressing takes no time at all, like a tenth of a millisecond.

---
+++ Ude/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Ude.NetStandard;
+internal static class Utils
+{
+    public static byte[] Decompress(byte[] bytes, int finalSize)
+    {
+        using var decompressedMS = new MemoryStream(finalSize);
+        using var compressedMS = new MemoryStream(bytes);
+        using var ds = new DeflateStream(compressedMS, CompressionMode.Decompress);
+        ds.CopyTo(decompressedMS);
+        return decompressedMS.GetBuffer();
+    }
+}
