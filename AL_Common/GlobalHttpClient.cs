d0efce76e01688d029e0900ca8540d98bf92511f
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-10-14 02:43:53 -0700

    Note about future perf

--- AL_Common/GlobalHttpClient.cs
+++ AL_Common/GlobalHttpClient.cs
@@ -4,6 +4,14 @@ namespace AL_Common;
 
 public static partial class Common
 {
+    /*
+    @TDM(Http):
+    Apparently on new .NETs there's a System.Net.Http.SocketsHttpHandler that apparently results in "a significant
+    performance improvement" compared to HttpClientHandler which Framework uses.
+    It's only in new .NETs, but there's a backport here: https://github.com/TalAloni/StandardSocketsHttpHandler
+    We can test this out later once we know what we're doing and see if we get better perf.
+    */
+
     // Only one instance is supposed to exist app-wide
     private static HttpClient? _globalHttpClient;
     public static HttpClient GlobalHttpClient => _globalHttpClient ??= new HttpClient();
