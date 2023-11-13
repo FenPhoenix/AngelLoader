e23936d23477675a305ddf67ad59c7eb047eb6d5
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-11-13 09:28:48 -0800

    More post_build.bat updates in prep for .NET 8

--- AngelLoader/post_build.bat
+++ AngelLoader/post_build.bat
@@ -47,6 +47,8 @@ if %PlatformName% == x86 (
 if "%TargetFramework:~0,4%"=="net4" (
 	del /F "%TargetDir%AngelLoader.dll"
 	del /F "%TargetDir%*.runtimeconfig.json"
+	del /F "%TargetDir%*.deps.json"
+	del /F "%TargetDir%System.Drawing.Primitives.dll"
 ) else (
 	del /F "%TargetDir%*.exe.config"
 	del /F "%TargetDir%System.Buffers.dll"
