04d5b3bddf40ca44f6dd405e1f6b2d2a8f6831fb
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-11-15 11:27:00 -0800

    Cleanup

--- SharpCompress/Readers/ReaderOptions.cs
+++ SharpCompress/Readers/ReaderOptions.cs
@@ -2,7 +2,7 @@ using SharpCompress.Common;
 
 namespace SharpCompress.Readers;
 
-public sealed class ReaderOptions : OptionsBase
+public sealed class ReaderOptions
 {
     /// <summary>
     /// Look for RarArchive (Check for self-extracting archives or cases where RarArchive isn't at the start of the file)
@@ -12,4 +12,11 @@ public sealed class ReaderOptions : OptionsBase
     public string? Password { get; set; }
 
     public bool DisableCheckIncomplete { get; set; }
+
+    /// <summary>
+    /// SharpCompress will keep the supplied streams open.  Default is true.
+    /// </summary>
+    public bool LeaveStreamOpen { get; set; } = true;
+
+    public ArchiveEncoding ArchiveEncoding { get; } = new ArchiveEncoding();
 }
