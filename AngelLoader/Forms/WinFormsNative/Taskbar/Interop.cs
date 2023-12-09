1c42229c88b8edd68ff5d3c89b7c51da67a5ce62
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-12-08 22:49:02 -0800

    Revert ITaskbarList3 to non-generated
    Although it seems to work, we get a warning, and the taskbar progress isn't supported for the new AOT-friendly COM stuff at all yet.

--- AngelLoader/Forms/WinFormsNative/Taskbar/Interop.cs
+++ AngelLoader/Forms/WinFormsNative/Taskbar/Interop.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿// @NET5(Taskbar progress): This thing isn't supported by the AOT-friendly COM interop generator thing yet.
+// It's not in CsWin32 either.
+// As of 2023-12-08.
+// So we're leaving it with the old style because we get a warning if we make this [GeneratedComInterface].
+
+using System;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.Marshalling;
 
 namespace AngelLoader.Forms.WinFormsNative.Taskbar;
 
-[GeneratedComInterface]
+[ComImport]
 [Guid("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
 [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-internal partial interface ITaskbarList3
+internal interface ITaskbarList3
 {
     // ITaskbarList
     [PreserveSig]
