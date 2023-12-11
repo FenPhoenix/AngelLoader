827e507fe41f6f871135a329ec7bae7a2b67a36f
FenPhoenix <FenPhoenix@users.noreply.github.com>
2023-12-10 20:45:09 -0800

    Start on "frugal object" work for CatAndTags

--- AngelLoader/Common/DataClasses/CatAndTags.cs
+++ AngelLoader/Common/DataClasses/CatAndTags.cs
@@ -2,41 +2,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using static AL_Common.Common;
 
 namespace AngelLoader.DataClasses;
 
-// @NET5/@MEM: We could do the "frugal object" pattern (https://prodotnetmemory.com/slides/PerformancePatternsLong/#79)
-// There's often zero or one tag in here, so it would reduce memory usage.
+/*
+@NET5/@MEM(CatAndTags "frugal object" pattern (https://prodotnetmemory.com/slides/PerformancePatternsLong/#79))
+@NET5(CatAndTags): Port this back to AL Framework when we've made this as frugal as we can.
+There's often zero or one tag in here, so it would reduce memory usage.
+
+2023-12-10:
+Some very basic frugal object stuff is now in here, but we get only a modest reduction. Many FMs still have at
+least "language: English", which is one category and one tag (two Lists, one HashSet, and one Dictionary).
+
+Ideas:
+-If we only have one item, we don't need any of the enumerables.
+ List = one string, HashSet = one string, Dictionary = one FMTagsCollection
+-Technically we could elide the FMTagsCollection entirely in that case, but callers are expecting an object of
+ that type. We'd need a wrapper object with just the one nullable FMTagsCollection field to elide the heavier
+ FMTagsCollection object. This would of course add more and more layers of indirection.
+*/
 public sealed class FMTagsCollection : IEnumerable<string>
 {
-    private readonly List<string> _list;
-    private readonly HashSetI _hashSet;
+    private readonly int _initialCapacity;
+    private List<string>? _list;
+    private HashSetI? _hashSet;
 
-    public FMTagsCollection()
+    private bool FieldsInitialized
     {
-        _list = new List<string>(0);
-        _hashSet = new HashSetI(0);
+        [MemberNotNullWhen(true, nameof(_hashSet), nameof(_list))]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set;
     }
 
-    public FMTagsCollection(int capacity)
+    [MemberNotNull(nameof(_hashSet), nameof(_list))]
+    private void InitializeFields()
     {
-        _list = new List<string>(capacity);
-        _hashSet = new HashSetI(capacity);
+        _list ??= new List<string>(_initialCapacity);
+        _hashSet ??= new HashSetI(_initialCapacity);
+        FieldsInitialized = true;
     }
 
-    public int Count => _list.Count;
+    public FMTagsCollection() => _initialCapacity = 1;
+
+    public FMTagsCollection(int capacity) => _initialCapacity = capacity;
+
+    public int Count => _list?.Count ?? 0;
 
     public void Clear()
     {
-        _hashSet.Clear();
-        _list.Clear();
+        _hashSet?.Clear();
+        _list?.Clear();
     }
 
-    public bool Contains(string item) => _hashSet.Contains(item);
+    public bool Contains(string item) => _hashSet?.Contains(item) ?? false;
 
     public void Add(string tag)
     {
+        InitializeFields();
+
         if (_hashSet.Add(tag))
         {
             _list.Add(tag);
@@ -45,28 +72,31 @@ public sealed class FMTagsCollection : IEnumerable<string>
 
     public void Remove(string category)
     {
+        if (!FieldsInitialized) return;
+
         _hashSet.Remove(category);
         _list.Remove(category);
     }
 
-#if false
-    public void RemoveAt(int index)
-    {
-        string item = _list[index];
-        _list.RemoveAt(index);
-        _hashSet.Remove(item);
-    }
-#endif
+    public string this[int index] => _list![index];
 
-    public string this[int index] => _list[index];
-
-    public void SortCaseInsensitive() => _list.Sort(StringComparer.OrdinalIgnoreCase);
+    public void SortCaseInsensitive() => _list?.Sort(StringComparer.OrdinalIgnoreCase);
 
     public IEnumerator<string> GetEnumerator()
     {
-        foreach (string item in _list)
+        if (FieldsInitialized)
         {
-            yield return item;
+            foreach (string item in _list)
+            {
+                yield return item;
+            }
+        }
+        else
+        {
+            foreach (string item in Array.Empty<string>())
+            {
+                yield return item;
+            }
         }
     }
 
@@ -87,25 +117,37 @@ public readonly struct CatAndTagsList
 
 public sealed class FMCategoriesCollection : IEnumerable<CatAndTagsList>
 {
-    private readonly DictionaryI<FMTagsCollection> _dict;
-    private readonly List<string> _list;
+    private readonly int _initialCapacity;
+    private List<string>? _list;
+    private DictionaryI<FMTagsCollection>? _dict;
 
-    public FMCategoriesCollection()
+    private bool FieldsInitialized
     {
-        _list = new List<string>(0);
-        _dict = new DictionaryI<FMTagsCollection>(0);
+        [MemberNotNullWhen(true, nameof(_dict), nameof(_list))]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set;
     }
 
-    public FMCategoriesCollection(int capacity)
+    [MemberNotNull(nameof(_dict), nameof(_list))]
+    private void InitializeFields()
     {
-        _list = new List<string>(capacity);
-        _dict = new DictionaryI<FMTagsCollection>(capacity);
+        _dict ??= new DictionaryI<FMTagsCollection>(_initialCapacity);
+        _list ??= new List<string>(_initialCapacity);
+        FieldsInitialized = true;
     }
 
-    public int Count => _list.Count;
+    public FMCategoriesCollection() => _initialCapacity = 1;
+
+    public FMCategoriesCollection(int capacity) => _initialCapacity = capacity;
+
+    public int Count => _list?.Count ?? 0;
 
     public void Add(string category, FMTagsCollection tags)
     {
+        InitializeFields();
+
         if (_dict.TryAdd(category, tags))
         {
             _list.Add(category);
@@ -114,40 +156,39 @@ public sealed class FMCategoriesCollection : IEnumerable<CatAndTagsList>
 
     public bool TryGetValue(string key, [NotNullWhen(true)] out FMTagsCollection? value)
     {
-        return _dict.TryGetValue(key, out value);
+        if (FieldsInitialized)
+        {
+            return _dict.TryGetValue(key, out value);
+        }
+        else
+        {
+            value = null;
+            return false;
+        }
     }
 
-    public bool ContainsKey(string key)
-    {
-        return _dict.ContainsKey(key);
-    }
+    public bool ContainsKey(string key) => _dict?.ContainsKey(key) ?? false;
 
     public bool Remove(string category)
     {
+        if (!FieldsInitialized) return false;
+
         _list.Remove(category);
         return _dict.Remove(category);
     }
 
-    public CatAndTagsList this[int index] => new(_list[index], _dict[_list[index]]);
-
-#if false
-    public bool RemoveAt(int index)
-    {
-        string item = _list[index];
-        _list.RemoveAt(index);
-        return _dict.Remove(item);
-    }
-#endif
+    public CatAndTagsList this[int index] => new(_list![index], _dict![_list![index]]);
 
     public void Clear()
     {
-        _dict.Clear();
-        _list.Clear();
+        _dict?.Clear();
+        _list?.Clear();
     }
 
     public void DeepCopyTo(FMCategoriesCollection dest)
     {
         dest.Clear();
+        if (!FieldsInitialized) return;
         for (int i = 0; i < _list.Count; i++)
         {
             string category = _list[i];
@@ -163,6 +204,8 @@ public sealed class FMCategoriesCollection : IEnumerable<CatAndTagsList>
 
     internal void SortAndMoveMiscToEnd()
     {
+        if (!FieldsInitialized) return;
+
         if (_list.Count == 0) return;
 
         _list.Sort(StringComparer.OrdinalIgnoreCase);
@@ -187,14 +230,24 @@ public sealed class FMCategoriesCollection : IEnumerable<CatAndTagsList>
     }
 
 #if false
-    public CatAndTagsList this[int index] => new CatAndTagsList(_list[index], _dict[_list[index]]);
+    public CatAndTagsList this[int index] => new CatAndTagsList(_list![index], _dict![_list![index]]);
 #endif
 
     public IEnumerator<CatAndTagsList> GetEnumerator()
     {
-        foreach (string item in _list)
+        if (FieldsInitialized)
         {
-            yield return new CatAndTagsList(item, _dict[item]);
+            foreach (string item in _list)
+            {
+                yield return new CatAndTagsList(item, _dict[item]);
+            }
+        }
+        else
+        {
+            foreach (string item in Array.Empty<string>())
+            {
+                yield return new CatAndTagsList(item, new FMTagsCollection());
+            }
         }
     }
 
