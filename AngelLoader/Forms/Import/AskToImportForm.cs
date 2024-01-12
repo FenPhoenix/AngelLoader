4aa55dc916ebff74d915d02a88bf9ec90e701ad2
FenPhoenix <FenPhoenix@users.noreply.github.com>
2024-01-12 10:15:13 -0800

    Port "Ask to import window is functional"

---
+++ AngelLoader/Forms/Import/AskToImportForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using AngelLoader.DataClasses;
+using static AngelLoader.Global;
+using static AngelLoader.Misc;
+
+namespace AngelLoader.Forms;
+public sealed partial class AskToImportForm : DarkFormBase
+{
+    public ImportType SelectedImportType;
+
+    public AskToImportForm()
+    {
+#if DEBUG
+        InitializeComponent();
+#else
+        InitSlim();
+#endif
+
+        if (Config.DarkMode) SetThemeBase(Config.VisualTheme);
+
+        Localize();
+    }
+
+    public override void RespondToSystemThemeChange() => SetThemeBase(Config.VisualTheme);
+
+    private void Localize()
+    {
+        Text = LText.Importing.AskToImport_Title;
+
+        MessageLabel.Text = LText.Importing.AskToImport_Message;
+        DarkLoaderButton.Text = NonLocalizableText.DarkLoaderEllipses;
+        FMSelButton.Text = NonLocalizableText.FMSelEllipses;
+        NewDarkLoaderButton.Text = NonLocalizableText.NewDarkLoaderEllipses;
+        DontImportButton.Text = LText.Importing.AskToImport_DontImport;
+
+        int maxButtonLength = 0;
+
+        if (DarkLoaderButton.Width > maxButtonLength) maxButtonLength = DarkLoaderButton.Width;
+        if (FMSelButton.Width > maxButtonLength) maxButtonLength = FMSelButton.Width;
+        if (NewDarkLoaderButton.Width > maxButtonLength) maxButtonLength = NewDarkLoaderButton.Width;
+        if (DontImportButton.Width > maxButtonLength) maxButtonLength = DontImportButton.Width;
+
+        DarkLoaderButton.Width = maxButtonLength;
+        FMSelButton.Width = maxButtonLength;
+        NewDarkLoaderButton.Width = maxButtonLength;
+        DontImportButton.Width = maxButtonLength;
+
+        int maxLength = 0;
+
+        if (MessageLabel.Width > maxLength) maxLength = MessageLabel.Width;
+        if (DarkLoaderButton.Width > maxLength) maxLength = DarkLoaderButton.Width;
+        if (FMSelButton.Width > maxLength) maxLength = FMSelButton.Width;
+        if (NewDarkLoaderButton.Width > maxLength) maxLength = NewDarkLoaderButton.Width;
+        if (DontImportButton.Width > maxLength) maxLength = DontImportButton.Width;
+
+        if (ClientSize.Width < maxLength + 48) ClientSize = ClientSize with { Width = maxLength + 48 };
+
+        MessageLabel.CenterH(this, clientSize: true);
+        DarkLoaderButton.CenterH(this, clientSize: true);
+        FMSelButton.CenterH(this, clientSize: true);
+        NewDarkLoaderButton.CenterH(this, clientSize: true);
+        DontImportButton.CenterH(this, clientSize: true);
+    }
+
+    private void ImportButtons_Click(object sender, EventArgs e)
+    {
+        SelectedImportType =
+            sender == DarkLoaderButton ? ImportType.DarkLoader :
+            sender == FMSelButton ? ImportType.FMSel :
+            ImportType.NewDarkLoader;
+        DialogResult = DialogResult.OK;
+        Close();
+    }
+
+    private void DontImportButton_Click(object sender, EventArgs e)
+    {
+        DialogResult = DialogResult.Cancel;
+        Close();
+    }
+}
---
+++ AngelLoader/Forms/Import/AskToImportForm_InitSlim.Generated.cs
@@ -0,0 +1,72 @@
+﻿namespace AngelLoader.Forms;
+
+sealed partial class AskToImportForm
+{
+    /// <summary>
+    /// Custom generated component initializer with cruft removed.
+    /// </summary>
+    private void InitSlim()
+    {
+        this.DarkLoaderButton = new AngelLoader.Forms.CustomControls.DarkButton();
+        this.FMSelButton = new AngelLoader.Forms.CustomControls.DarkButton();
+        this.NewDarkLoaderButton = new AngelLoader.Forms.CustomControls.DarkButton();
+        this.DontImportButton = new AngelLoader.Forms.CustomControls.DarkButton();
+        this.MessageLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
+        this.SuspendLayout();
+        // 
+        // DarkLoaderButton
+        // 
+        this.DarkLoaderButton.Location = new System.Drawing.Point(96, 48);
+        this.DarkLoaderButton.Size = new System.Drawing.Size(112, 23);
+        this.DarkLoaderButton.TabIndex = 1;
+        this.DarkLoaderButton.Click += new System.EventHandler(this.ImportButtons_Click);
+        // 
+        // FMSelButton
+        // 
+        this.FMSelButton.Location = new System.Drawing.Point(96, 72);
+        this.FMSelButton.Size = new System.Drawing.Size(112, 23);
+        this.FMSelButton.TabIndex = 2;
+        this.FMSelButton.Click += new System.EventHandler(this.ImportButtons_Click);
+        // 
+        // NewDarkLoaderButton
+        // 
+        this.NewDarkLoaderButton.Location = new System.Drawing.Point(96, 96);
+        this.NewDarkLoaderButton.Size = new System.Drawing.Size(112, 23);
+        this.NewDarkLoaderButton.TabIndex = 3;
+        this.NewDarkLoaderButton.Click += new System.EventHandler(this.ImportButtons_Click);
+        // 
+        // DontImportButton
+        // 
+        this.DontImportButton.AutoSize = true;
+        this.DontImportButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        this.DontImportButton.Location = new System.Drawing.Point(96, 128);
+        this.DontImportButton.TabIndex = 4;
+        this.DontImportButton.Click += new System.EventHandler(this.DontImportButton_Click);
+        // 
+        // MessageLabel
+        // 
+        this.MessageLabel.AutoSize = true;
+        this.MessageLabel.Location = new System.Drawing.Point(24, 16);
+        this.MessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        // 
+        // AskToImportForm
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.CancelButton = this.DontImportButton;
+        this.ClientSize = new System.Drawing.Size(302, 167);
+        this.Controls.Add(this.MessageLabel);
+        this.Controls.Add(this.DontImportButton);
+        this.Controls.Add(this.NewDarkLoaderButton);
+        this.Controls.Add(this.FMSelButton);
+        this.Controls.Add(this.DarkLoaderButton);
+        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        this.MaximizeBox = false;
+        this.MinimizeBox = false;
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+        // Hack to prevent slow first render on some forms if Text is blank
+        this.Text = " ";
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+}
