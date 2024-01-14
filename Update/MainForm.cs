0054bb05036fdec86fbcfc3eb7af9362354adfbb
FenPhoenix <FenPhoenix@users.noreply.github.com>
2024-01-14 14:22:27 -0800

    Organize updater

--- Update/MainForm.cs
+++ Update/MainForm.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace Update;
@@ -35,78 +31,20 @@ public sealed partial class MainForm : Form
 #endif
 
         Text = "AngelLoader Update";
-        CopyingLabel.Text = "Copying...";
-
-        CopyingLabel.CenterHOnForm(this);
         CopyingProgressBar.CenterHOnForm(this);
     }
 
     protected override void OnShown(EventArgs e)
     {
         base.OnShown(e);
-        DoCopy();
+        Program.DoCopy();
     }
 
-    private void DoCopy()
+    public void SetMessage(string message)
     {
-        string startupPath = Application.StartupPath;
-        string exePath = Application.ExecutablePath;
-
-        try
-        {
-            File.Delete(exePath + ".bak");
-        }
-        catch
-        {
-            // didn't exist or whatever
-        }
-
-        File.Move(exePath, exePath + ".bak");
-
-        List<string> files = Directory.GetFiles(Program.UpdateTempPath, "*", SearchOption.AllDirectories).ToList();
-
-        for (int i = 0; i < files.Count; i++)
-        {
-            string fileName = Path.GetFileName(files[i]);
-
-            if (fileName.EqualsI("FMData.ini") ||
-                fileName.StartsWithI("FMData.bak") ||
-                fileName.EqualsI("Config.ini"))
-            {
-                files.RemoveAt(i);
-                i--;
-            }
-        }
-
-        string updateDirWithTrailingDirSep = Program.UpdateTempPath.TrimEnd('\\', '/') + "\\";
-
-        for (int i = 0; i < files.Count; i++)
-        {
-            string file = files[i];
-            string fileName = file.Substring(updateDirWithTrailingDirSep.Length);
-
-            CopyingLabel.Text = "Copying..." + Environment.NewLine + fileName;
-            CopyingLabel.CenterHOnForm(this);
-
-            // TODO: Handle errors robustly
-            string finalFileName = Path.Combine(startupPath, fileName);
-            Directory.CreateDirectory(Path.GetDirectoryName(finalFileName)!);
-            File.Copy(file, finalFileName, overwrite: true);
-
-            //for (int t = 0; t < 100; t++)
-            //{
-            //    Thread.Sleep(1);
-            //    Application.DoEvents();
-            //}
-
-            int percent = Utils.GetPercentFromValue_Int(i + 1, files.Count);
-            CopyingProgressBar.SetProgress(percent);
-        }
-
-        Utils.ClearUpdateTempPath();
-
-        // TODO: Handle errors robustly
-        using (Process.Start(Path.Combine(startupPath, "AngelLoader.exe"))) { }
-        Application.Exit();
+        CopyingLabel.Text = message;
+        CopyingLabel.CenterHOnForm(this);
     }
+
+    public void SetProgress(int percent) => CopyingProgressBar.SetProgress(percent);
 }
--- Update/Program.cs
+++ Update/Program.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using Microsoft.VisualBasic.ApplicationServices;
 
@@ -7,9 +10,6 @@ namespace Update;
 
 internal static class Program
 {
-    private static readonly string _baseTempPath = Path.Combine(Path.GetTempPath(), "AngelLoader");
-    internal static readonly string UpdateTempPath = Path.Combine(_baseTempPath, "Update");
-
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
@@ -27,8 +27,76 @@ internal static class Program
 
         protected override bool OnStartup(StartupEventArgs eventArgs)
         {
-            Application.Run(new MainForm());
+            MainView = new MainForm();
+            Application.Run(MainView);
             return false;
         }
+    }
+
+    private static MainForm MainView = null!;
+
+    private static readonly string _baseTempPath = Path.Combine(Path.GetTempPath(), "AngelLoader");
+    internal static readonly string UpdateTempPath = Path.Combine(_baseTempPath, "Update");
+
+    internal static void DoCopy()
+    {
+        string startupPath = Application.StartupPath;
+        string exePath = Application.ExecutablePath;
+
+        try
+        {
+            File.Delete(exePath + ".bak");
+        }
+        catch
+        {
+            // didn't exist or whatever
+        }
+
+        File.Move(exePath, exePath + ".bak");
+
+        List<string> files = Directory.GetFiles(UpdateTempPath, "*", SearchOption.AllDirectories).ToList();
+
+        for (int i = 0; i < files.Count; i++)
+        {
+            string fileName = Path.GetFileName(files[i]);
+
+            if (fileName.EqualsI("FMData.ini") ||
+                fileName.StartsWithI("FMData.bak") ||
+                fileName.EqualsI("Config.ini"))
+            {
+                files.RemoveAt(i);
+                i--;
+            }
+        }
+
+        string updateDirWithTrailingDirSep = UpdateTempPath.TrimEnd('\\', '/') + "\\";
+
+        for (int i = 0; i < files.Count; i++)
+        {
+            string file = files[i];
+            string fileName = file.Substring(updateDirWithTrailingDirSep.Length);
+
+            MainView.SetMessage("Copying..." + Environment.NewLine + fileName);
+
+            // TODO: Handle errors robustly
+            string finalFileName = Path.Combine(startupPath, fileName);
+            Directory.CreateDirectory(Path.GetDirectoryName(finalFileName)!);
+            File.Copy(file, finalFileName, overwrite: true);
+
+            //for (int t = 0; t < 100; t++)
+            //{
+            //    Thread.Sleep(1);
+            //    Application.DoEvents();
+            //}
+
+            int percent = Utils.GetPercentFromValue_Int(i + 1, files.Count);
+            MainView.SetProgress(percent);
+        }
+
+        Utils.ClearUpdateTempPath();
+
+        // TODO: Handle errors robustly
+        using (Process.Start(Path.Combine(startupPath, "AngelLoader.exe"))) { }
+        Application.Exit();
     }
 }
