2e4097160cf873f63f91b1c63dc4360f1994e10f
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-02-04 04:34:22 -0800

    More paths cleanup

--- Update/Paths.cs
+++ Update/Paths.cs
@@ -5,13 +5,13 @@ namespace Update;
 
 internal static class Paths
 {
-    // @Update: We could/maybe should generate AL's constants into a file in this project.
-    // That way we'll automatically update them here without having to have a dll dependency (which is a no-go).
+    // These are manual duplicates from the main app, but it's highly unlikely any of them will change, for
+    // backward compatibility reasons if nothing else.
     internal static readonly string LogFile = Path.Combine(Application.StartupPath, "AngelLoader_log.txt");
 
     internal static readonly string ConfigIni = Path.Combine(Application.StartupPath, "Data", "Config.ini");
 
-    internal static readonly string _baseTemp = Path.Combine(Path.GetTempPath(), "AngelLoader");
+    private static readonly string _baseTemp = Path.Combine(Path.GetTempPath(), "AngelLoader");
     internal static readonly string UpdateTemp = Path.Combine(_baseTemp, "Update");
     internal static readonly string UpdateBakTemp = Path.Combine(_baseTemp, "UpdateBak");
 }
