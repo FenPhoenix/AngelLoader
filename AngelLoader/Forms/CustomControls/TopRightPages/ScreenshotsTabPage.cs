0e5ff0cfa4ef274565f13d29f055d7f2d9858213
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-02-22 11:38:27 -0800

    Implement prev/next, and disable caching for now
    Let's finish the feature before we add the perf complexity

--- AngelLoader/Forms/CustomControls/TopRightPages/ScreenshotsTabPage.cs
+++ AngelLoader/Forms/CustomControls/TopRightPages/ScreenshotsTabPage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using AL_Common;
 using AngelLoader.DataClasses;
 using JetBrains.Annotations;
 
@@ -12,7 +11,7 @@ public sealed class ScreenshotsTabPage : Lazy_TabsBase
     private Lazy_ScreenshotsPage _page = null!;
 
     private readonly List<string> ScreenshotFileNames = new();
-    private string StoredScreenshotFileName = "";
+    private string CurrentScreenshotFileName = "";
 
     #region Theme
 
@@ -67,53 +66,54 @@ public sealed class ScreenshotsTabPage : Lazy_TabsBase
     {
         if (!_constructed) return;
 
-        if (!_owner.StartupState && !Visible) return;
-
         FanMission? fm = _owner.GetMainSelectedFMOrNull();
 
         Core.PopulateScreenshotFileNames(fm, ScreenshotFileNames);
 
         if (ScreenshotFileNames.Count == 0)
         {
-            StoredScreenshotFileName = "";
+            CurrentScreenshotFileName = "";
             _page.ScreenshotsPictureBox.Image = null;
             _page.ScreenshotsPictureBox.Enabled = false;
             _page.ScreenshotsPrevButton.Enabled = false;
             _page.ScreenshotsNextButton.Enabled = false;
         }
         // @ScreenshotDisplay: Should we save the selected screenshot in the FM object?
-        // @TDM_CASE (when the FM is TDM)
-        else if (!StoredScreenshotFileName.EqualsI(ScreenshotFileNames[0]))
+        else
         {
-            // @ScreenshotDisplay: We can't just use the 0th element, we need to use the actual selected one
-            StoredScreenshotFileName = ScreenshotFileNames[0];
-            _page.ScreenshotsPictureBox.Load(StoredScreenshotFileName);
+            CurrentScreenshotFileName = ScreenshotFileNames[0];
+            DisplayCurrentScreenshot();
             _page.ScreenshotsPictureBox.Enabled = true;
             _page.ScreenshotsPrevButton.Enabled = ScreenshotFileNames.Count > 1;
             _page.ScreenshotsNextButton.Enabled = ScreenshotFileNames.Count > 1;
         }
     }
 
-    protected override void OnVisibleChanged(EventArgs e)
+    private void DisplayCurrentScreenshot()
     {
-        base.OnVisibleChanged(e);
-
-        if (!_owner.StartupState && Visible)
-        {
-            UpdatePage();
-        }
+        _page.ScreenshotsPictureBox.Load(CurrentScreenshotFileName);
     }
 
     #region Page
 
-    private void ScreenshotsPrevButton_Click(object sender, EventArgs e)
-    {
-        // @ScreenshotDisplay: Implement
-    }
+    private void ScreenshotsPrevButton_Click(object sender, EventArgs e) => CycleScreenshot(step: -1);
 
-    private void ScreenshotsNextButton_Click(object sender, EventArgs e)
+    private void ScreenshotsNextButton_Click(object sender, EventArgs e) => CycleScreenshot(step: 1);
+
+    private void CycleScreenshot(int step)
     {
-        // @ScreenshotDisplay: Implement
+        if (ScreenshotFileNames.Count <= 1) return;
+        int index = ScreenshotFileNames.IndexOf(CurrentScreenshotFileName);
+
+        if (index == -1) return;
+
+        index =
+            step == 1
+                ? index == ScreenshotFileNames.Count - 1 ? 0 : index + 1
+                : index == 0 ? ScreenshotFileNames.Count - 1 : index - 1;
+
+        CurrentScreenshotFileName = ScreenshotFileNames[index];
+        DisplayCurrentScreenshot();
     }
 
     #endregion
