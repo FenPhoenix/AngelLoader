4ad36fcdb802cc2869081119c5c7bc1d69e316bb
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-02-23 14:07:48 -0800

    Picture box resizes with tab resizing

--- AngelLoader/Forms/CustomControls/TopRightPages/Lazy_ScreenshotsPage.Designer.cs
+++ AngelLoader/Forms/CustomControls/TopRightPages/Lazy_ScreenshotsPage.Designer.cs
@@ -31,51 +31,58 @@ public sealed partial class Lazy_ScreenshotsPage
     /// </summary>
     private void InitializeComponent()
     {
-            this.ScreenshotsPictureBox = new AngelLoader.Forms.CustomControls.DarkPictureBox();
+            this.NumberLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
             this.ScreenshotsNextButton = new AngelLoader.Forms.CustomControls.DarkArrowButton();
             this.ScreenshotsPrevButton = new AngelLoader.Forms.CustomControls.DarkArrowButton();
-            this.NumberLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
+            this.ScreenshotsPictureBox = new AngelLoader.Forms.CustomControls.DarkPictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.ScreenshotsPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // ScreenshotsPictureBox
+            // NumberLabel
             // 
-            this.ScreenshotsPictureBox.Location = new System.Drawing.Point(8, 8);
-            this.ScreenshotsPictureBox.Name = "ScreenshotsPictureBox";
-            this.ScreenshotsPictureBox.Size = new System.Drawing.Size(328, 192);
-            this.ScreenshotsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.ScreenshotsPictureBox.TabIndex = 0;
-            this.ScreenshotsPictureBox.TabStop = false;
+            this.NumberLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Location = new System.Drawing.Point(8, 260);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(0, 13);
+            this.NumberLabel.TabIndex = 0;
             // 
             // ScreenshotsNextButton
             // 
+            this.ScreenshotsNextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ScreenshotsNextButton.ArrowDirection = AngelLoader.Forms.Direction.Right;
-            this.ScreenshotsNextButton.Location = new System.Drawing.Point(262, 200);
+            this.ScreenshotsNextButton.Location = new System.Drawing.Point(446, 256);
             this.ScreenshotsNextButton.Name = "ScreenshotsNextButton";
             this.ScreenshotsNextButton.Size = new System.Drawing.Size(75, 23);
             this.ScreenshotsNextButton.TabIndex = 2;
             // 
             // ScreenshotsPrevButton
             // 
+            this.ScreenshotsPrevButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ScreenshotsPrevButton.ArrowDirection = AngelLoader.Forms.Direction.Left;
-            this.ScreenshotsPrevButton.Location = new System.Drawing.Point(187, 200);
+            this.ScreenshotsPrevButton.Location = new System.Drawing.Point(371, 256);
             this.ScreenshotsPrevButton.Name = "ScreenshotsPrevButton";
             this.ScreenshotsPrevButton.Size = new System.Drawing.Size(75, 23);
             this.ScreenshotsPrevButton.TabIndex = 1;
             // 
-            // NumberLabel
+            // ScreenshotsPictureBox
             // 
-            this.NumberLabel.AutoSize = true;
-            this.NumberLabel.Location = new System.Drawing.Point(8, 204);
-            this.NumberLabel.Name = "NumberLabel";
-            this.NumberLabel.Size = new System.Drawing.Size(0, 13);
-            this.NumberLabel.TabIndex = 0;
+            this.ScreenshotsPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ScreenshotsPictureBox.Location = new System.Drawing.Point(8, 8);
+            this.ScreenshotsPictureBox.Name = "ScreenshotsPictureBox";
+            this.ScreenshotsPictureBox.Size = new System.Drawing.Size(512, 248);
+            this.ScreenshotsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ScreenshotsPictureBox.TabIndex = 0;
+            this.ScreenshotsPictureBox.TabStop = false;
             // 
             // Lazy_ScreenshotsPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(220, 100);
             this.Controls.Add(this.NumberLabel);
             this.Controls.Add(this.ScreenshotsNextButton);
             this.Controls.Add(this.ScreenshotsPrevButton);
