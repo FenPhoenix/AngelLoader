31b063f9c560df25c291ce3a2823888102cdbdd2
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-03-05 22:15:03 -0800

    Patch tab now scales well with resizing
    Instead of the old "everything stays small lol" thing

--- AngelLoader/Forms/CustomControls/TopRightPages/Lazy_PatchPage_InitSlim.Generated.cs
+++ AngelLoader/Forms/CustomControls/TopRightPages/Lazy_PatchPage_InitSlim.Generated.cs
@@ -13,13 +13,15 @@ public sealed partial class Lazy_PatchPage
         this.Patch_NewMantle_CheckBox = new AngelLoader.Forms.CustomControls.DarkCheckBox();
         this.PatchMainPanel = new AngelLoader.Forms.CustomControls.DrawnPanel();
         this.PatchDMLsPanel = new AngelLoader.Forms.CustomControls.DrawnPanel();
+        this.AddRemoveDMLButtonsFLP = new AngelLoader.Forms.CustomControls.DarkFlowLayoutPanel();
+        this.PatchAddDMLButton = new AngelLoader.Forms.CustomControls.DarkButton();
+        this.PatchRemoveDMLButton = new AngelLoader.Forms.CustomControls.DarkButton();
         this.PatchDMLPatchesLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
         this.PatchDMLsListBox = new AngelLoader.Forms.CustomControls.DarkListBox();
-        this.PatchRemoveDMLButton = new AngelLoader.Forms.CustomControls.DarkButton();
-        this.PatchAddDMLButton = new AngelLoader.Forms.CustomControls.DarkButton();
         this.PatchOpenFMFolderButton = new AngelLoader.Forms.CustomControls.DarkButton();
         this.PatchMainPanel.SuspendLayout();
         this.PatchDMLsPanel.SuspendLayout();
+        this.AddRemoveDMLButtonsFLP.SuspendLayout();
         this.SuspendLayout();
         // 
         // Patch_PerFMValues_Label
@@ -56,25 +58,51 @@ public sealed partial class Lazy_PatchPage
         // 
         // PatchMainPanel
         // 
-        this.PatchMainPanel.AutoSize = true;
+        this.PatchMainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+        | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
         this.PatchMainPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
         this.PatchMainPanel.Controls.Add(this.PatchDMLsPanel);
         this.PatchMainPanel.Controls.Add(this.PatchOpenFMFolderButton);
         this.PatchMainPanel.Location = new System.Drawing.Point(0, 104);
-        this.PatchMainPanel.Size = new System.Drawing.Size(307, 250);
+        this.PatchMainPanel.Size = new System.Drawing.Size(527, 250);
         this.PatchMainPanel.TabIndex = 43;
         // 
         // PatchDMLsPanel
         // 
-        this.PatchDMLsPanel.AutoSize = true;
+        this.PatchDMLsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+        | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
         this.PatchDMLsPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        this.PatchDMLsPanel.Controls.Add(this.AddRemoveDMLButtonsFLP);
         this.PatchDMLsPanel.Controls.Add(this.PatchDMLPatchesLabel);
         this.PatchDMLsPanel.Controls.Add(this.PatchDMLsListBox);
-        this.PatchDMLsPanel.Controls.Add(this.PatchRemoveDMLButton);
-        this.PatchDMLsPanel.Controls.Add(this.PatchAddDMLButton);
         this.PatchDMLsPanel.Location = new System.Drawing.Point(-2, 0);
-        this.PatchDMLsPanel.Size = new System.Drawing.Size(306, 218);
+        this.PatchDMLsPanel.Size = new System.Drawing.Size(527, 218);
         this.PatchDMLsPanel.TabIndex = 39;
+        // 
+        // AddRemoveDMLButtonsFLP
+        // 
+        this.AddRemoveDMLButtonsFLP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
+        this.AddRemoveDMLButtonsFLP.Controls.Add(this.PatchAddDMLButton);
+        this.AddRemoveDMLButtonsFLP.Controls.Add(this.PatchRemoveDMLButton);
+        this.AddRemoveDMLButtonsFLP.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+        this.AddRemoveDMLButtonsFLP.Location = new System.Drawing.Point(6, 193);
+        this.AddRemoveDMLButtonsFLP.Size = new System.Drawing.Size(519, 24);
+        this.AddRemoveDMLButtonsFLP.TabIndex = 44;
+        // 
+        // PatchAddDMLButton
+        // 
+        this.PatchAddDMLButton.Margin = new System.Windows.Forms.Padding(1, 0, 0, 0);
+        this.PatchAddDMLButton.Size = new System.Drawing.Size(23, 23);
+        this.PatchAddDMLButton.TabIndex = 43;
+        // 
+        // PatchRemoveDMLButton
+        // 
+        this.PatchRemoveDMLButton.Margin = new System.Windows.Forms.Padding(1, 0, 0, 0);
+        this.PatchRemoveDMLButton.Size = new System.Drawing.Size(23, 23);
+        this.PatchRemoveDMLButton.TabIndex = 42;
         // 
         // PatchDMLPatchesLabel
         // 
@@ -83,25 +111,17 @@ public sealed partial class Lazy_PatchPage
         // 
         // PatchDMLsListBox
         // 
+        this.PatchDMLsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+        | System.Windows.Forms.AnchorStyles.Left)
+        | System.Windows.Forms.AnchorStyles.Right)));
         this.PatchDMLsListBox.Location = new System.Drawing.Point(6, 24);
         this.PatchDMLsListBox.MultiSelect = false;
-        this.PatchDMLsListBox.Size = new System.Drawing.Size(296, 168);
+        this.PatchDMLsListBox.Size = new System.Drawing.Size(518, 168);
         this.PatchDMLsListBox.TabIndex = 41;
-        // 
-        // PatchRemoveDMLButton
-        // 
-        this.PatchRemoveDMLButton.Location = new System.Drawing.Point(256, 192);
-        this.PatchRemoveDMLButton.Size = new System.Drawing.Size(23, 23);
-        this.PatchRemoveDMLButton.TabIndex = 42;
-        // 
-        // PatchAddDMLButton
-        // 
-        this.PatchAddDMLButton.Location = new System.Drawing.Point(280, 192);
-        this.PatchAddDMLButton.Size = new System.Drawing.Size(23, 23);
-        this.PatchAddDMLButton.TabIndex = 43;
         // 
         // PatchOpenFMFolderButton
         // 
+        this.PatchOpenFMFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
         this.PatchOpenFMFolderButton.AutoSize = true;
         this.PatchOpenFMFolderButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
         this.PatchOpenFMFolderButton.Location = new System.Drawing.Point(5, 224);
@@ -113,6 +133,7 @@ public sealed partial class Lazy_PatchPage
         this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
         this.AutoScroll = true;
+        this.AutoScrollMinSize = new System.Drawing.Size(200, 300);
         this.Controls.Add(this.Patch_PerFMValues_Label);
         this.Controls.Add(this.Patch_NDSubs_CheckBox);
         this.Controls.Add(this.Patch_PostProc_CheckBox);
@@ -123,6 +144,7 @@ public sealed partial class Lazy_PatchPage
         this.PatchMainPanel.PerformLayout();
         this.PatchDMLsPanel.ResumeLayout(false);
         this.PatchDMLsPanel.PerformLayout();
+        this.AddRemoveDMLButtonsFLP.ResumeLayout(false);
         this.ResumeLayout(false);
         this.PerformLayout();
     }
