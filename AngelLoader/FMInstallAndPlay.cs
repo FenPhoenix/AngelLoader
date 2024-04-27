9fa2fef42fd9f551b0d65d79356f19e7be561169
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-04-26 23:03:13 -0700

    Improve an error message

--- AngelLoader/FMInstallAndPlay.cs
+++ AngelLoader/FMInstallAndPlay.cs
@@ -1464,8 +1464,7 @@ internal static partial class FMInstallAndPlay
                 {
                     if (!File.Exists(gameExe))
                     {
-                        Log("Game executable not found.\r\n" +
-                            "Game executable: " + gameExe);
+                        fm.LogInfo("Game executable not found.\r\nGame executable: " + gameExe);
                         Core.Dialogs.ShowError(gameName + ":\r\n" +
                                                fm.GetId() + "\r\n" +
                                                LText.AlertMessages.Install_ExecutableNotFound);
