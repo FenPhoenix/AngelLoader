e69eb13aaea5a4a64b5e3c1da2d0598c59b8fdf1
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-06-17 02:01:41 -0700

    ProgressPanel: Fix cancel button anchoring

--- AngelLoader/Forms/CustomControls/ProgressPanel_InitSlim.Generated.cs
+++ AngelLoader/Forms/CustomControls/ProgressPanel_InitSlim.Generated.cs
@@ -7,96 +7,99 @@ sealed partial class ProgressPanel
     /// </summary>
     private void InitSlim()
     {
-        this.SubProgressBar = new AngelLoader.Forms.CustomControls.DarkProgressBar();
-        this.Cancel_Button = new AngelLoader.Forms.CustomControls.DarkButton();
-        this.SubPercentLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
-        this.MainPercentLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
-        this.MainMessage1Label = new AngelLoader.Forms.CustomControls.DarkLabel();
-        this.MainProgressBar = new AngelLoader.Forms.CustomControls.DarkProgressBar();
-        this.SubMessageLabel = new AngelLoader.Forms.CustomControls.DarkLabel();
-        this.MainMessage2Label = new AngelLoader.Forms.CustomControls.DarkLabel();
-        this.SuspendLayout();
+        SubProgressBar = new DarkProgressBar();
+        Cancel_Button = new DarkButton();
+        SubPercentLabel = new DarkLabel();
+        MainPercentLabel = new DarkLabel();
+        MainMessage1Label = new DarkLabel();
+        MainProgressBar = new DarkProgressBar();
+        SubMessageLabel = new DarkLabel();
+        MainMessage2Label = new DarkLabel();
+        SuspendLayout();
         // 
         // SubProgressBar
         // 
-        this.SubProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.SubProgressBar.Location = new System.Drawing.Point(8, 120);
-        this.SubProgressBar.Size = new System.Drawing.Size(406, 16);
+        SubProgressBar.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        SubProgressBar.Location = new System.Drawing.Point(9, 138);
+        SubProgressBar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        SubProgressBar.Size = new System.Drawing.Size(474, 18);
         // 
         // Cancel_Button
         // 
-        this.Cancel_Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-        this.Cancel_Button.AutoSize = true;
-        this.Cancel_Button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-        this.Cancel_Button.Location = new System.Drawing.Point(168, 152);
-        this.Cancel_Button.MinimumSize = new System.Drawing.Size(88, 23);
-        this.Cancel_Button.Padding = new System.Windows.Forms.Padding(6, 0, 6, 0);
-        this.Cancel_Button.TabIndex = 7;
-        this.Cancel_Button.Click += new System.EventHandler(this.ProgressCancelButton_Click);
+        Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+        Cancel_Button.AutoSize = true;
+        Cancel_Button.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        Cancel_Button.Location = new System.Drawing.Point(196, 175);
+        Cancel_Button.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        Cancel_Button.MinimumSize = new System.Drawing.Size(103, 27);
+        Cancel_Button.Padding = new System.Windows.Forms.Padding(7, 0, 7, 0);
+        Cancel_Button.TabIndex = 7;
+        Cancel_Button.Click += ProgressCancelButton_Click;
         // 
         // SubPercentLabel
         // 
-        this.SubPercentLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.SubPercentLabel.Location = new System.Drawing.Point(4, 104);
-        this.SubPercentLabel.Size = new System.Drawing.Size(416, 13);
-        this.SubPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        SubPercentLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        SubPercentLabel.Location = new System.Drawing.Point(5, 120);
+        SubPercentLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+        SubPercentLabel.Size = new System.Drawing.Size(485, 15);
+        SubPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
         // 
         // MainPercentLabel
         // 
-        this.MainPercentLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.MainPercentLabel.Location = new System.Drawing.Point(4, 40);
-        this.MainPercentLabel.Size = new System.Drawing.Size(416, 13);
-        this.MainPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        MainPercentLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        MainPercentLabel.Location = new System.Drawing.Point(5, 46);
+        MainPercentLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+        MainPercentLabel.Size = new System.Drawing.Size(485, 15);
+        MainPercentLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
         // 
         // MainMessage1Label
         // 
-        this.MainMessage1Label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.MainMessage1Label.Location = new System.Drawing.Point(4, 8);
-        this.MainMessage1Label.Size = new System.Drawing.Size(416, 13);
-        this.MainMessage1Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        MainMessage1Label.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        MainMessage1Label.Location = new System.Drawing.Point(5, 9);
+        MainMessage1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+        MainMessage1Label.Size = new System.Drawing.Size(485, 15);
+        MainMessage1Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
         // 
         // MainProgressBar
         // 
-        this.MainProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.MainProgressBar.Location = new System.Drawing.Point(8, 56);
-        this.MainProgressBar.Size = new System.Drawing.Size(406, 23);
+        MainProgressBar.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        MainProgressBar.Location = new System.Drawing.Point(9, 65);
+        MainProgressBar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        MainProgressBar.Size = new System.Drawing.Size(474, 27);
         // 
         // SubMessageLabel
         // 
-        this.SubMessageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.SubMessageLabel.Location = new System.Drawing.Point(4, 88);
-        this.SubMessageLabel.Size = new System.Drawing.Size(416, 13);
-        this.SubMessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        SubMessageLabel.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        SubMessageLabel.Location = new System.Drawing.Point(5, 102);
+        SubMessageLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+        SubMessageLabel.Size = new System.Drawing.Size(485, 15);
+        SubMessageLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
         // 
         // MainMessage2Label
         // 
-        this.MainMessage2Label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-        | System.Windows.Forms.AnchorStyles.Right)));
-        this.MainMessage2Label.Location = new System.Drawing.Point(4, 24);
-        this.MainMessage2Label.Size = new System.Drawing.Size(416, 13);
-        this.MainMessage2Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        MainMessage2Label.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+        MainMessage2Label.Location = new System.Drawing.Point(5, 28);
+        MainMessage2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+        MainMessage2Label.Size = new System.Drawing.Size(485, 15);
+        MainMessage2Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
         // 
         // ProgressPanel
         // 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-        this.Controls.Add(this.SubProgressBar);
-        this.Controls.Add(this.Cancel_Button);
-        this.Controls.Add(this.SubPercentLabel);
-        this.Controls.Add(this.MainPercentLabel);
-        this.Controls.Add(this.MainMessage1Label);
-        this.Controls.Add(this.MainProgressBar);
-        this.Controls.Add(this.SubMessageLabel);
-        this.Controls.Add(this.MainMessage2Label);
-        this.Size = new System.Drawing.Size(424, 192);
-        this.ResumeLayout(false);
-        this.PerformLayout();
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        Controls.Add(SubProgressBar);
+        Controls.Add(Cancel_Button);
+        Controls.Add(SubPercentLabel);
+        Controls.Add(MainPercentLabel);
+        Controls.Add(MainMessage1Label);
+        Controls.Add(MainProgressBar);
+        Controls.Add(SubMessageLabel);
+        Controls.Add(MainMessage2Label);
+        Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        Name = "ProgressPanel";
+        Size = new System.Drawing.Size(495, 222);
+        ResumeLayout(false);
+        PerformLayout();
     }
 }
