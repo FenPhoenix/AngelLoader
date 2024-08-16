e5c4a0ea0c04e140e8c40c4b2d5b89e93758ef22
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-08-15 23:20:36 -0700

    Typo

--- AngelLoader/Common/DataClasses/NonLocalizableText.cs
+++ AngelLoader/Common/DataClasses/NonLocalizableText.cs
@@ -24,7 +24,7 @@ internal static partial class NonLocalizableText
         "copies of the Software, and to permit persons to whom the Software is " +
         $"furnished to do so, subject to the following conditions:{NL}{NL}" +
         "The above copyright notice and this permission notice shall be included in all " +
-        $"copies or substantial portions of the Software{NL}{NL}" +
+        $"copies or substantial portions of the Software.{NL}{NL}" +
         "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR " +
         "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, " +
         "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL " +
