6c19cc259de7adb2092436f43de4d01f9cbeb269
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-08-29 01:11:49 -0700

    Notes

--- AngelLoader/Core.cs
+++ AngelLoader/Core.cs
@@ -1048,10 +1048,12 @@ internal static class Core
                     View.SetWaitCursor(true);
                 }
 
-                // @MT_TASK: Runs on UI thread and blocks it for too long (potentially)
                 List<string> paths = new(sourceConfig.FMArchivePaths.Count + SupportedGameCount);
                 paths.AddRange_Small(sourceConfig.FMArchivePaths);
                 paths.AddRange_Small(sourceConfig.GameExes);
+                // @MT_TASK: Runs on UI thread and blocks it for too long (potentially)
+                // @MT_TASK: We could run this in a thread in the Settings window and wait on it before exit
+                // Or after exit if we pass back the task or whatever, if we wanted to be fancy
                 destConfig.AllDrivesAreSSD = DetectDriveTypes.AllDrivesAreSolidState(paths);
             }
             finally
