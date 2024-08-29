10b7f3213446be5698012defbca0d1d4f6336b6d
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-08-29 03:25:58 -0700

    Notes

--- AngelLoader/Forms/Settings/AdvancedPage.cs
+++ AngelLoader/Forms/Settings/AdvancedPage.cs
@@ -5,7 +5,8 @@ namespace AngelLoader.Forms;
 public sealed partial class AdvancedPage : UserControl, Interfaces.ISettingsPage
 {
     // @MT_TASK: In addition to "Auto", do we want like a "Max" option, or even just a "Max" button by the threads field?
-
+    // @MT_TASK: Should we display on the UI the number of threads Auto has detected?
+    // @MT_TASK: We should have help messages on the UI explaining that we use 1 thread for HDDs and why.
     public AdvancedPage()
     {
 #if DEBUG
