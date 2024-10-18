16cbdebc9c31210716904511b59e5cadc1174a92
Brian Tobin <FenPhoenix@users.noreply.github.com>
2024-10-18 00:16:33 -0700

    Remove compressed ude arrays code

--- Ude/Utils.cs
+++ Ude/Utils.cs
@@ -2,17 +2,6 @@
 
 internal static class Utils
 {
-#if CompressUdeDataArrays
-    internal static byte[] Decompress(byte[] bytes, int finalSize)
-    {
-        using var decompressedMS = new System.IO.MemoryStream(finalSize);
-        using var compressedMS = new System.IO.MemoryStream(bytes);
-        using var ds = new System.IO.Compression.DeflateStream(compressedMS, System.IO.Compression.CompressionMode.Decompress);
-        ds.CopyTo(decompressedMS);
-        return decompressedMS.GetBuffer();
-    }
-#endif
-
     internal static int UnpackBitPackage(int[] data, int i)
     {
         const int INDEX_SHIFT_4BITS = 3;
