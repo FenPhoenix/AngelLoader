3f1fffc17a9d89b4ba8962f7e07b9a888afb642e
F <20834294+FenPhoenix@users.noreply.github.com>
2024-11-09 03:26:28 -0800

    Fix reg key dispose, and remove note

--- AngelLoader/WinVersion.cs
+++ AngelLoader/WinVersion.cs
@@ -68,14 +68,7 @@ internal static class WinVersion
     }
 
     /*
-    @MT_TASK(Attention - Win11 tooltip behavior change):
-    Apparently Windows 11 tooltips are now persistent if AutoPopDelay has not been changed. However, "persistent"
-    means not even mouse movement will make them disappear, according to this issue: https://github.com/dotnet/winforms/issues/12374
-
-    So, we could leave this in and get that behavior, or we could set 32767 and get the previous behavior:
-    long enough to read it, but also disappears when the mouse moves like any sane person would want it.
-
-    Info source: https://learn.microsoft.com/en-us/dotnet/framework/whats-new/whats-new-in-accessibility#winforms481
+    https://learn.microsoft.com/en-us/dotnet/framework/whats-new/whats-new-in-accessibility#winforms481
     "ToolTip now follows WCAG2.1 guidelines to be persistent, dismissable, and hoverable on Windows 11.
     Changes to tooltip behavior are limited to Windows 11 systems that have .NET Framework 4.8.1 installed,
     and only apply to applications where a timeout was not set for the tooltip. Tooltips that are persisting
@@ -86,9 +79,8 @@ internal static class WinVersion
     {
         try
         {
-            using var ndpKey = RegistryKey
-                .OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default)
-                .OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\");
+            using RegistryKey? hklm = (RegistryKey?)RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Default);
+            using RegistryKey? ndpKey = hklm?.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\", writable: false);
             return ndpKey?.GetValue("Release") is >= 533320 && Is11OrAbove;
         }
         catch
