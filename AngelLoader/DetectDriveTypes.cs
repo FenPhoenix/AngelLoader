d407e1c11db2e888f35dd5fa925c76881c8d6e17
F <20834294+FenPhoenix@users.noreply.github.com>
2024-11-13 13:14:13 -0800

    Reorganize DetectDriveTypes in prep for Settings style detection

--- AngelLoader/DetectDriveTypes.cs
+++ AngelLoader/DetectDriveTypes.cs
@@ -78,97 +78,7 @@ internal static class DetectDriveTypes
 
         foreach (string root in roots)
         {
-            AL_DriveType driveType;
-
-            // We've got a network drive or something else, and we don't know what it is.
-            if (!RootIsLetter(root))
-            {
-                driveType = AL_DriveType.Other;
-            }
-            else
-            {
-                SafeFileHandle? safeHandle = null;
-                try
-                {
-                    // I can't really test to see if this is even needed in this situation, but let's just use it
-                    // for safety. We REALLY don't want some prompt window popping up when we're running this;
-                    // users won't know wtf's going on.
-                    using (DisableMediaInsertionPrompt.Create())
-                    {
-                        string dummyFileName = @"\\.\" + root;
-                        safeHandle = CreateFileW(
-                            lpFileName: dummyFileName,
-                            /*
-                            IMPORTANT(Drive type detect non-administrator bullet dodge):
-                            Access ***MUST*** be set to 0! If we set any other access at all, then the operation
-                            will require administrator privileges. The ONLY way we can run on non-admin is to set
-                            0 here!
-                            */
-                            dwDesiredAccess: 0,
-                            dwShareMode: FileShare.ReadWrite,
-                            lpSecurityAttributes: IntPtr.Zero,
-                            dwCreationDisposition: FileMode.Open,
-                            dwFlagsAndAttributes: FileAttributes.Normal,
-                            hTemplateFile: IntPtr.Zero);
-                    }
-
-                    StorageDeviceWrapper device = new(safeHandle);
-
-                    /*
-                    @MT_TASK: This can fail (for USB/Optical (virtual mounted)/SD (through USB dongle)/etc.)
-                    If it fails, we fall back to Other which is probably fine. But, we could also say if this
-                    fails then we should continue on below and get the bus type, from which we may be able to
-                    make a more intelligent decision.
-
-                    It's possible to get the nominal rotation rate, but this requires admin privileges. The docs
-                    make it sound like it doesn't:
-
-                    https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/ntddscsi/ni-ntddscsi-ioctl_ata_pass_through
-                    "Applications do not require administrative privileges to send a pass-through request to a
-                    device, but they must have read/write access to the device."
-
-                    But of course, having read/write access to the device itself requires admin privileges, so
-                    the fact that something beyond it doesn't is completely useless. Oh well.
-                    */
-                    DEVICE_SEEK_PENALTY_DESCRIPTOR seekPenaltyDescriptor = device.StorageGetSeekPenaltyDescriptor();
-                    if (seekPenaltyDescriptor.IncursSeekPenalty)
-                    {
-                        driveType = AL_DriveType.Other;
-                    }
-                    else
-                    {
-                        STORAGE_DEVICE_DESCRIPTOR_PARSED deviceProperty = device.StorageGetDeviceProperty();
-
-                        driveType = deviceProperty.BusType switch
-                        {
-                            STORAGE_BUS_TYPE.BusTypeNvme
-                                or STORAGE_BUS_TYPE.BusTypeSCM
-                                /*
-                                @MT_TASK: The question of RAID
-                                We could have SATA RAID or NVMe RAID, and we don't know which... we also don't
-                                know if it's striped or mirrored. Striped SATA RAID is probably fast enough for
-                                aggressive threading, but mirrored RAID is the same speed as non-RAID. So we're
-                                in a bit of a conundrum here.
-                                */
-                                //or STORAGE_BUS_TYPE.BusTypeRAID
-                                => AL_DriveType.NVMe_SSD,
-                            STORAGE_BUS_TYPE.BusTypeSata => AL_DriveType.SATA_SSD,
-                            // We know we have no seek penalty, so "SATA SSD" should be a safe minimum level for
-                            // exotic bus types.
-                            _ => AL_DriveType.SATA_SSD,
-                        };
-                    }
-                }
-                catch
-                {
-                    driveType = AL_DriveType.Other;
-                }
-                finally
-                {
-                    safeHandle?.Dispose();
-                }
-            }
-
+            AL_DriveType driveType = GetDriveType(root);
             ret.Add(driveType);
         }
 
@@ -188,78 +98,112 @@ internal static class DetectDriveTypes
             }
             return ret;
         }
+    }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static bool RootIsLetter(string root)
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    private static extern SafeFileHandle CreateFileW(
+        string lpFileName,
+        [MarshalAs(UnmanagedType.U4)] FileAccess dwDesiredAccess,
+        [MarshalAs(UnmanagedType.U4)] FileShare dwShareMode,
+        IntPtr lpSecurityAttributes,
+        [MarshalAs(UnmanagedType.U4)] FileMode dwCreationDisposition,
+        [MarshalAs(UnmanagedType.U4)] FileAttributes dwFlagsAndAttributes,
+        IntPtr hTemplateFile);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool RootIsLetter(string root)
+    {
+        return !root.IsEmpty() && root.Length == 2 && root[0].IsAsciiAlpha() && root[1] == ':';
+    }
+
+    private static AL_DriveType GetDriveType(string root)
+    {
+        // We've got a network drive or something else, and we don't know what it is.
+        if (!RootIsLetter(root))
         {
-            return !root.IsEmpty() && root.Length == 2 && root[0].IsAsciiAlpha() && root[1] == ':';
+            return AL_DriveType.Other;
         }
-
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-        static extern SafeFileHandle CreateFileW(
-            string lpFileName,
-            [MarshalAs(UnmanagedType.U4)] FileAccess dwDesiredAccess,
-            [MarshalAs(UnmanagedType.U4)] FileShare dwShareMode,
-            IntPtr lpSecurityAttributes,
-            [MarshalAs(UnmanagedType.U4)] FileMode dwCreationDisposition,
-            [MarshalAs(UnmanagedType.U4)] FileAttributes dwFlagsAndAttributes,
-            IntPtr hTemplateFile);
-
-        // In case the directory is a symlink pointing at some other drive
-        static IOPath GetRealDirectory(IOPath path)
+        else
         {
+            SafeFileHandle? safeHandle = null;
             try
             {
-                if (path.Type == IOPathType.File)
+                // I can't really test to see if this is even needed in this situation, but let's just use it
+                // for safety. We REALLY don't want some prompt window popping up when we're running this;
+                // users won't know wtf's going on.
+                using (DisableMediaInsertionPrompt.Create())
                 {
-                    string? dir = Path.GetDirectoryName(path.Path);
-                    if (dir.IsEmpty()) return path;
-                    path = new IOPath(dir, IOPathType.Directory);
+                    string dummyFileName = @"\\.\" + root;
+                    safeHandle = CreateFileW(
+                        lpFileName: dummyFileName,
+                        /*
+                        IMPORTANT(Drive type detect non-administrator bullet dodge):
+                        Access ***MUST*** be set to 0! If we set any other access at all, then the operation
+                        will require administrator privileges. The ONLY way we can run on non-admin is to set
+                        0 here!
+                        */
+                        dwDesiredAccess: 0,
+                        dwShareMode: FileShare.ReadWrite,
+                        lpSecurityAttributes: IntPtr.Zero,
+                        dwCreationDisposition: FileMode.Open,
+                        dwFlagsAndAttributes: FileAttributes.Normal,
+                        hTemplateFile: IntPtr.Zero);
                 }
 
-                DirectoryInfo di = new(path.Path);
-                if (!di.Exists)
-                {
-                    return path;
-                }
+                StorageDeviceWrapper device = new(safeHandle);
 
-                string? realPath;
+                /*
+                @MT_TASK: This can fail (for USB/Optical (virtual mounted)/SD (through USB dongle)/etc.)
+                If it fails, we fall back to Other which is probably fine. But, we could also say if this
+                fails then we should continue on below and get the bus type, from which we may be able to
+                make a more intelligent decision.
 
-                // Perf: Checking for symbolic link is expensive(double-digit milliseconds for one check), so
-                // just do a reparse point check first, which is effectively instantaneous.
-                if (!IsReparsePoint(di))
+                It's possible to get the nominal rotation rate, but this requires admin privileges. The docs
+                make it sound like it doesn't:
+
+                https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/ntddscsi/ni-ntddscsi-ioctl_ata_pass_through
+                "Applications do not require administrative privileges to send a pass-through request to a
+                device, but they must have read/write access to the device."
+
+                But of course, having read/write access to the device itself requires admin privileges, so
+                the fact that something beyond it doesn't is completely useless. Oh well.
+                */
+                DEVICE_SEEK_PENALTY_DESCRIPTOR seekPenaltyDescriptor = device.StorageGetSeekPenaltyDescriptor();
+                if (seekPenaltyDescriptor.IncursSeekPenalty)
                 {
-                    if (TryGetSubstedPath(path.Path, out realPath))
-                    {
-                        return new IOPath(realPath, IOPathType.Directory);
-                    }
-                    return path;
+                    return AL_DriveType.Other;
                 }
                 else
                 {
-                    if (TryGetSubstedPath(path.Path, out realPath))
+                    STORAGE_DEVICE_DESCRIPTOR_PARSED deviceProperty = device.StorageGetDeviceProperty();
+
+                    return deviceProperty.BusType switch
                     {
-                        if (!IsReparsePoint(new DirectoryInfo(path.Path)))
-                        {
-                            return new IOPath(realPath, IOPathType.Directory);
-                        }
-                        path = new IOPath(realPath, IOPathType.Directory);
-                    }
+                        STORAGE_BUS_TYPE.BusTypeNvme
+                            or STORAGE_BUS_TYPE.BusTypeSCM
+                            /*
+                            @MT_TASK: The question of RAID
+                            We could have SATA RAID or NVMe RAID, and we don't know which... we also don't
+                            know if it's striped or mirrored. Striped SATA RAID is probably fast enough for
+                            aggressive threading, but mirrored RAID is the same speed as non-RAID. So we're
+                            in a bit of a conundrum here.
+                            */
+                            //or STORAGE_BUS_TYPE.BusTypeRAID
+                            => AL_DriveType.NVMe_SSD,
+                        STORAGE_BUS_TYPE.BusTypeSata => AL_DriveType.SATA_SSD,
+                        // We know we have no seek penalty, so "SATA SSD" should be a safe minimum level for
+                        // exotic bus types.
+                        _ => AL_DriveType.SATA_SSD,
+                    };
                 }
-
-                FileSystemInfo? fsi = Directory_ResolveLinkTarget(path.Path, returnFinalTarget: true);
-                if (fsi != null)
-                {
-                    path = new IOPath(fsi.FullName, IOPathType.Directory);
-                }
-
-                return path;
-
-                static bool IsReparsePoint(DirectoryInfo di) => (di.Attributes & FileAttributes.ReparsePoint) != 0;
             }
             catch
             {
-                return path;
+                return AL_DriveType.Other;
+            }
+            finally
+            {
+                safeHandle?.Dispose();
             }
         }
     }
@@ -347,4 +291,63 @@ internal static class DetectDriveTypes
 
     private static bool PathStartsWithLetterAndVolumeSeparator(string path) =>
         path.Length >= 2 && path[0].IsAsciiAlpha() && path[1] == Path.VolumeSeparatorChar;
+
+
+    // In case the directory is a symlink pointing at some other drive
+    private static IOPath GetRealDirectory(IOPath path)
+    {
+        try
+        {
+            if (path.Type == IOPathType.File)
+            {
+                string? dir = Path.GetDirectoryName(path.Path);
+                if (dir.IsEmpty()) return path;
+                path = new IOPath(dir, IOPathType.Directory);
+            }
+
+            DirectoryInfo di = new(path.Path);
+            if (!di.Exists)
+            {
+                return path;
+            }
+
+            string? realPath;
+
+            // Perf: Checking for symbolic link is expensive(double-digit milliseconds for one check), so
+            // just do a reparse point check first, which is effectively instantaneous.
+            if (!IsReparsePoint(di))
+            {
+                if (TryGetSubstedPath(path.Path, out realPath))
+                {
+                    return new IOPath(realPath, IOPathType.Directory);
+                }
+                return path;
+            }
+            else
+            {
+                if (TryGetSubstedPath(path.Path, out realPath))
+                {
+                    if (!IsReparsePoint(new DirectoryInfo(path.Path)))
+                    {
+                        return new IOPath(realPath, IOPathType.Directory);
+                    }
+                    path = new IOPath(realPath, IOPathType.Directory);
+                }
+            }
+
+            FileSystemInfo? fsi = Directory_ResolveLinkTarget(path.Path, returnFinalTarget: true);
+            if (fsi != null)
+            {
+                path = new IOPath(fsi.FullName, IOPathType.Directory);
+            }
+
+            return path;
+
+            static bool IsReparsePoint(DirectoryInfo di) => (di.Attributes & FileAttributes.ReparsePoint) != 0;
+        }
+        catch
+        {
+            return path;
+        }
+    }
 }
