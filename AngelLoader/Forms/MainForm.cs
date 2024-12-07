32ef9745b5210ad41c516f6f812302274e9758ca
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2024-12-06 22:11:42 -0800

    Eh?

--- AngelLoader/Forms/MainForm.cs
+++ AngelLoader/Forms/MainForm.cs
@@ -36,7 +36,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
