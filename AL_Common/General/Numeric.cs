6fd88362d5902644b417799994dd6e202ce55d53
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2024-12-09 01:09:07 -0800

    Zip: Reduce common-case size of 64k filename and data buffers

--- AL_Common/General/Numeric.cs
+++ AL_Common/General/Numeric.cs
@@ -99,4 +99,51 @@ public static partial class Common
     }
 
     #endregion
+
+    #region Round up to power of 2
+
+    // From modern .NET
+
+    /// <summary>Round the given integral value up to a power of 2.</summary>
+    /// <param name="value">The value.</param>
+    /// <returns>
+    /// The smallest power of 2 which is greater than or equal to <paramref name="value"/>.
+    /// If <paramref name="value"/> is 0 or the result overflows, returns 0.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static uint RoundUpToPowerOf2(uint value)
+    {
+        // Based on https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        --value;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+        return value + 1;
+    }
+
+    /// <summary>
+    /// Round the given integral value up to a power of 2.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <returns>
+    /// The smallest power of 2 which is greater than or equal to <paramref name="value"/>.
+    /// If <paramref name="value"/> is 0 or the result overflows, returns 0.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ulong RoundUpToPowerOf2(ulong value)
+    {
+        // Based on https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        --value;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+        value |= value >> 32;
+        return value + 1;
+    }
+
+    #endregion
 }
