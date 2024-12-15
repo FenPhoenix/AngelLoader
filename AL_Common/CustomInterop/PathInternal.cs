092bec56d7ba31486ca3deb320cce337437899c7
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2024-12-14 17:11:31 -0800

    Cleanup

--- AL_Common/CustomInterop/PathInternal.cs
+++ AL_Common/CustomInterop/PathInternal.cs
@@ -3,7 +3,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 #if ENABLE_UNUSED
 using System.Diagnostics;
 #endif
--- AL_Common/IO/StreamReaderCustom.cs
+++ AL_Common/IO/StreamReaderCustom.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
-
 #if ENABLE_UNUSED
 using System.Runtime.InteropServices;
 #endif
