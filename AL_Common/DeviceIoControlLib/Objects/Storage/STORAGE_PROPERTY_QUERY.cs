322706c76b7a7645789cf1a92f0643b842ce80c7
FenPhoenix <FenPhoenix@users.noreply.github.com>
2024-12-26 05:27:00 -0800

    Urgent notes about maybe making a mistake in DeviceIoControl stuff

--- AL_Common/DeviceIoControlLib/Objects/Storage/STORAGE_PROPERTY_QUERY.cs
+++ AL_Common/DeviceIoControlLib/Objects/Storage/STORAGE_PROPERTY_QUERY.cs
@@ -8,5 +8,7 @@ public struct STORAGE_PROPERTY_QUERY
 {
     public STORAGE_PROPERTY_ID PropertyId;
     public STORAGE_QUERY_TYPE QueryType;
+    // @NET5/IMPORTANT/BUG: This is part of a setup that may be incorrect and cause crashes on some systems!
+    //                      The Framework version is a byte[].
     public IntPtr AdditionalParameters;
 }
