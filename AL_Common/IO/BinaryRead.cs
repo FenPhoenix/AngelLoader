965400d4f92ae163060393647c1486e008872b79
FenPhoenix <FenPhoenix@users.noreply.github.com>
2025-03-12 11:30:09 -0700

    BinaryRead efficiency

--- AL_Common/IO/BinaryRead.cs
+++ AL_Common/IO/BinaryRead.cs
@@ -1,5 +1,5 @@
-﻿using System.Buffers.Binary;
-using System.IO;
+﻿using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace AL_Common;
@@ -17,22 +17,37 @@ public readonly struct BinaryBuffer()
 /// </summary>
 public static class BinaryRead
 {
+    /// <summary>Reads a 2-byte unsigned integer from the current stream using little-endian encoding and advances the position of the stream by two bytes.</summary>
+    /// <returns>A 2-byte unsigned integer read from this stream.</returns>
+    /// <exception cref="T:System.IO.EndOfStreamException">The end of the stream is reached.</exception>
+    /// <exception cref="T:System.ObjectDisposedException">The stream is closed.</exception>
+    /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
     public static ushort ReadUInt16(Stream stream, BinaryBuffer buffer)
     {
         ReadInternal(stream, buffer.Buffer, 2);
-        return BinaryPrimitives.ReadUInt16LittleEndian(buffer.Buffer);
+        return Unsafe.ReadUnaligned<ushort>(ref buffer.Buffer[0]);
     }
 
+    /// <summary>Reads a 4-byte unsigned integer from the current stream and advances the position of the stream by four bytes.</summary>
+    /// <returns>A 4-byte unsigned integer read from this stream.</returns>
+    /// <exception cref="T:System.IO.EndOfStreamException">The end of the stream is reached.</exception>
+    /// <exception cref="T:System.ObjectDisposedException">The stream is closed.</exception>
+    /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
     public static uint ReadUInt32(Stream stream, BinaryBuffer buffer)
     {
         ReadInternal(stream, buffer.Buffer, 4);
-        return BinaryPrimitives.ReadUInt32LittleEndian(buffer.Buffer);
+        return Unsafe.ReadUnaligned<uint>(ref buffer.Buffer[0]);
     }
 
+    /// <summary>Reads an 8-byte unsigned integer from the current stream and advances the position of the stream by eight bytes.</summary>
+    /// <returns>An 8-byte unsigned integer read from this stream.</returns>
+    /// <exception cref="T:System.IO.EndOfStreamException">The end of the stream is reached.</exception>
+    /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
+    /// <exception cref="T:System.ObjectDisposedException">The stream is closed.</exception>
     public static ulong ReadUInt64(Stream stream, BinaryBuffer buffer)
     {
         ReadInternal(stream, buffer.Buffer, 8);
-        return BinaryPrimitives.ReadUInt64LittleEndian(buffer.Buffer);
+        return Unsafe.ReadUnaligned<ulong>(ref buffer.Buffer[0]);
     }
 
     private static void ReadInternal(Stream stream, byte[] buffer, int count)
