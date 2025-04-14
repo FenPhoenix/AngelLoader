1943cf99c31399e660ae8cd71ab9a8411f80e481
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2025-04-14 00:04:52 -0700

    Cleanup

--- AL_Common/DeviceIoControlLib/Utilities/MarshalHelper.cs
+++ AL_Common/DeviceIoControlLib/Utilities/MarshalHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace AL_Common.DeviceIoControlLib.Utilities;
 
--- AL_Common/IO/FastIO_Native.cs
+++ AL_Common/IO/FastIO_Native.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
