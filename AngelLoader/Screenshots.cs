c0c6dd04773ebf220fb556e9d01c1c993e321426
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2025-04-21 15:43:29 -0700

    Dispose screenshots watcher on exception in construct code

--- AngelLoader/Screenshots.cs
+++ AngelLoader/Screenshots.cs
@@ -107,6 +107,10 @@ public sealed class ScreenshotWatcher(GameIndex gameIndex)
         }
         catch
         {
+            if (_watcher != null!)
+            {
+                _watcher.Dispose();
+            }
             _constructed = false;
             return;
         }
