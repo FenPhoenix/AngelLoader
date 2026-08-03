fa1bea24dcde0c6d777753a483a969a03b5a57f6
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2026-08-03 10:22:50 -0700

    Cleanup

--- AngelLoader/Forms/FormsData.cs
+++ AngelLoader/Forms/FormsData.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Windows.Forms;
 using AngelLoader.DataClasses;
 using AngelLoader.Forms.CustomControls;
