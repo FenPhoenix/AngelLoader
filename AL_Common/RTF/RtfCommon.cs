006769f033674c2804bbcebf8dd6a502cac9f69f
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2026-08-19 17:46:05 -0700

    Move Symbol to its own file

--- AL_Common/RTF/RtfCommon.cs
+++ AL_Common/RTF/RtfCommon.cs
@@ -736,26 +736,4 @@ public static class RtfCommon
         FCharset,
         CPG,
     }
-
-    internal sealed class Symbol
-    {
-        internal readonly string Keyword;
-        internal readonly int DefaultParam;
-        internal readonly bool UseDefaultParam;
-        internal readonly KeywordType KeywordType;
-        /// <summary>
-        /// Index into the property table, or a regular enum member, or a character literal, depending on <see cref="KeywordType"/>.
-        /// </summary>
-        internal readonly ushort Index;
-
-        internal Symbol(string keyword, int defaultParam, bool useDefaultParam, KeywordType keywordType, ushort index)
-        {
-            Keyword = keyword;
-            DefaultParam = defaultParam;
-            UseDefaultParam = useDefaultParam;
-            KeywordType = keywordType;
-            Index = index;
-        }
-    }
-
 }
---
+++ AL_Common/RTF/Symbol.cs
@@ -0,0 +1,24 @@
+﻿using static AL_Common.RTF.RtfCommon;
+
+namespace AL_Common.RTF;
+
+internal sealed class Symbol
+{
+    internal readonly string Keyword;
+    internal readonly int DefaultParam;
+    internal readonly bool UseDefaultParam;
+    internal readonly KeywordType KeywordType;
+    /// <summary>
+    /// Index into the property table, or a regular enum member, or a character literal, depending on <see cref="KeywordType"/>.
+    /// </summary>
+    internal readonly ushort Index;
+
+    internal Symbol(string keyword, int defaultParam, bool useDefaultParam, KeywordType keywordType, ushort index)
+    {
+        Keyword = keyword;
+        DefaultParam = defaultParam;
+        UseDefaultParam = useDefaultParam;
+        KeywordType = keywordType;
+        Index = index;
+    }
+}
