a1f0c0262bcf6805f21d8bc386c070c45e730296
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2026-08-26 19:03:25 -0700

    Remove done todo

--- AngelLoader/Rtf/GLMLConversion.cs
+++ AngelLoader/Rtf/GLMLConversion.cs
@@ -6,12 +6,6 @@ using AngelLoader.DataClasses;
 
 namespace AngelLoader;
 
-/*
-@GLML(Character encoding): Some GLML files have non-ASCII chars directly in them, rather than specified with an
-HTML numeric specifier or whatnot. We're not handling these correctly. We may need to run encoding detection on
-them.
-*/
-
 internal static class GLMLConversion
 {
     [SuppressMessage("ReSharper", "StringLiteralTypo")]
