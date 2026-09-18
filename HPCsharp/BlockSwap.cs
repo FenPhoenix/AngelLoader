e11064be98596d7520c9f3706c4c857fbfc267f2
FenPhoenix <20834294+FenPhoenix@users.noreply.github.com>
2026-09-18 13:43:32 -0700

    Compacting HPCsharp

--- HPCsharp/BlockSwap.cs
+++ HPCsharp/BlockSwap.cs
@@ -20,31 +20,6 @@ namespace HPCsharp
 {
     public static partial class Algorithm
     {
-        public static void Swap<T>(this T[] array, int indexA, int indexB, int length, bool reverse = false)
-        {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
-            if (!reverse)
-            {
-                while (length-- > 0)
-                {
-                    T temp          = array[indexA];                 // inlining Swap() increases performance by 25%
-                    array[indexA++] = array[indexB];
-                    array[indexB++] = temp;
-                }
-            }
-            else
-            {
-                int currIndexB = indexB + length - 1;
-                while (length-- > 0)
-                {
-                    T temp              = array[indexA];             // inlining Swap() increases performance by 25%
-                    array[indexA++]     = array[currIndexB];
-                    array[currIndexB--] = temp;
-                }
-            }
-        }
-
         public static void Swap<T>(ref T a, ref T b)
         {
             T temp = a;
@@ -63,47 +38,6 @@ namespace HPCsharp
                 array[l] = array[r];
                 array[r] = temp;
             }
-        }
-
-        // Swaps two sequential subarrays ranges a[ l .. m ] and a[ m + 1 .. r ]
-        public static void BlockSwapReversal<T>(T[] array, int l, int m, int r, int threshold = 1024)
-        {
-            if ((r - l) < threshold)
-            {
-                array.Reversal(l,     m);
-                array.Reversal(m + 1, r);
-                array.Reversal(l,     r);
-            }
-            else
-            {
-                Array.Reverse(array, l,     m - l + 1);   // 2X slower than array.Reversal when used in In-Place Merge Sort, but is 2X faster when this funciton is benchmarked by itself
-                Array.Reverse(array, m + 1, r - m    );   // Theory: Array.Reverse() has large overhead => does not peform well for small arrays
-                Array.Reverse(array, l,     r - l + 1);
-            }
-        }
-
-        public static void BlockSwapGriesMills<T>(T[] array, int l, int m, int r)
-        {
-            int rotdist = m - l + 1;
-            int n       = r - l + 1;
-            if (rotdist == 0 || rotdist == n) return;
-            int p, i = p = rotdist;
-            int j = n - p;
-            p += l;
-            while (i != j)
-            {
-                if (i > j)
-                {
-                    array.Swap(p - i, p, j);
-                    i -= j;
-                }
-                else
-                {
-                    array.Swap(p - i, p + j - i, i);
-                    j -= i;
-                }
-            }
-            array.Swap(p - i, p, i);
         }
     }
 }
